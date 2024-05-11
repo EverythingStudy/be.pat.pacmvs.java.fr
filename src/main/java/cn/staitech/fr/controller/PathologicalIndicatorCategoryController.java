437ae413d21f0bce988f7bcaae585ed86bfea120
耿江涛 <3207883183@qq.com>
2024-05-11 17:20:09 +0800

    获取单脏器下所有标签更改

--- src/main/java/cn/staitech/fr/controller/PathologicalIndicatorCategoryController.java
+++ src/main/java/cn/staitech/fr/controller/PathologicalIndicatorCategoryController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Api(value = "脏器标签", tags = "脏器标签")
@@ -46,15 +47,52 @@ public class PathologicalIndicatorCategoryController {
     @GetMapping("/selectCategoryList")
     public R<List<PathologicalIndicatorCategory>> getDataList(@RequestParam(value = "specialId") @ApiParam(name = "specialId", value = "专题id", required = true) Long specialId, @RequestParam(value = "categoryId") @ApiParam(name = "categoryId", value = "脏器标签id", required = true) Long categoryId) {
         String organId = categoryService.getById(categoryId).getOrganId();
+        if(organId == null){
+            return R.ok(new ArrayList<>());
+        }
+        List<String> organList = new ArrayList<>();
+        switch (organId) {
+            case "7D":
+                organList.add("3E");
+                organList.add("3D");
+                break;
+            case "7C":
+                organList.add("24");
+                organList.add("25");
+                break;
+            case "7B":
+                organList.add("5F");
+                organList.add("3F");
+                break;
+            case "7A":
+                organList.add("21");
+                organList.add("23");
+                break;
+            case "53":
+                organList.add("50");
+                organList.add("51");
+                break;
+            default:
+                organList.add(organId);
+                break;
+        }
         Long speciesId = Long.valueOf(specialService.getById(specialId).getSpeciesId());
         QueryWrapper<PathologicalIndicator> pathologicalIndicatorQueryWrapper = new QueryWrapper<>();
-        pathologicalIndicatorQueryWrapper.eq("species_id", speciesId).eq("organ_id", organId).eq("organization_id",SecurityUtils.getLoginUser().getSysUser().getOrganizationId()).eq("del_flag",0);
-        PathologicalIndicator indicator = pathologicalIndicatorService.getOne(pathologicalIndicatorQueryWrapper);
-        if(indicator == null){
+        pathologicalIndicatorQueryWrapper
+                .eq("del_flag", 0)
+                .eq("species_id", speciesId)
+                .eq("organization_id", SecurityUtils.getLoginUser().getSysUser().getOrganizationId())
+                .in("organ_id", organList);
+        List<PathologicalIndicator> indicator = pathologicalIndicatorService.list(pathologicalIndicatorQueryWrapper);
+        if (indicator == null) {
+            return R.ok(new ArrayList<>());
+        }
+        List<Long> indicatorIds = indicator.stream().map(PathologicalIndicator::getIndicatorId).collect(Collectors.toList());
+        if(indicatorIds.size() == 0){
             return R.ok(new ArrayList<>());
         }
         QueryWrapper<PathologicalIndicatorCategory> pathologicalIndicatorCategoryQueryWrapper = new QueryWrapper<>();
-        pathologicalIndicatorCategoryQueryWrapper.eq("indicator_id", indicator.getIndicatorId());
+        pathologicalIndicatorCategoryQueryWrapper.in("indicator_id", indicatorIds);
         return R.ok(pathologicalIndicatorCategoryService.list(pathologicalIndicatorCategoryQueryWrapper));
     }
 
