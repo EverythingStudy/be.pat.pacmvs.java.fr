58caf1ad79fc5638a36cbfd7965ee3361e8be7d0
wanglibei <wanglb@staitech.cn>
2024-05-16 13:18:16 +0800

    胸骨

--- src/main/java/cn/staitech/fr/service/strategy/json/impl/OvariesOviductParserStrategyImpl.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/OvariesOviductParserStrategyImpl.java
@@ -1,45 +1,29 @@
 package cn.staitech.fr.service.strategy.json.impl;
 
-import cn.hutool.core.collection.CollectionUtil;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import cn.hutool.core.util.ObjectUtil;
-import cn.staitech.common.core.utils.SpringUtils;
-import cn.staitech.fr.domain.*;
+import cn.staitech.fr.domain.AiForecast;
+import cn.staitech.fr.domain.Annotation;
+import cn.staitech.fr.domain.JsonTask;
+import cn.staitech.fr.domain.PathologicalIndicatorCategory;
+import cn.staitech.fr.domain.SpecialAnnotationRel;
 import cn.staitech.fr.mapper.AnnotationMapper;
 import cn.staitech.fr.mapper.ImageMapper;
 import cn.staitech.fr.mapper.PathologicalIndicatorCategoryMapper;
 import cn.staitech.fr.mapper.SingleSlideMapper;
 import cn.staitech.fr.mapper.SpecialAnnotationRelMapper;
 import cn.staitech.fr.service.AiForecastService;
-import cn.staitech.fr.service.strategy.json.ParserStrategy;
-import cn.staitech.fr.vo.geojson.Indicator;
-import cn.staitech.fr.vo.geojson.Properties;
-import com.alibaba.fastjson.JSONObject;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.JsonToken;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import cn.staitech.fr.service.strategy.json.AbstractCustomParserStrategy;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 /**
  * 
@@ -51,224 +35,33 @@ import java.util.stream.Collectors;
  */
 @Slf4j
 @Component("Ovaries")
-public class OvariesOviductParserStrategyImpl implements ParserStrategy {
+public  class OvariesOviductParserStrategyImpl extends AbstractCustomParserStrategy {
 
 	@Resource
-	public SpecialAnnotationRelMapper specialAnnotationRelMapper = SpringUtils.getBean(SpecialAnnotationRelMapper.class);
-	@Resource
-	private PathologicalIndicatorCategoryMapper pathologicalIndicatorCategoryMapper = SpringUtils.getBean(PathologicalIndicatorCategoryMapper.class);
-	@Resource
-	private AnnotationMapper annotationMapper = SpringUtils.getBean(AnnotationMapper.class);
-	@Resource
-	private SingleSlideMapper singleSlideMapper = SpringUtils.getBean(SingleSlideMapper.class);
-	@Resource
-	private AiForecastService aiForecastService = SpringUtils.getBean(AiForecastService.class);
-	@Resource
-	public ImageMapper imageMapper = SpringUtils.getBean(ImageMapper.class);
+    private SpecialAnnotationRelMapper specialAnnotationRelMapper;
+    @Resource
+    private PathologicalIndicatorCategoryMapper pathologicalIndicatorCategoryMapper;
+    @Resource
+    private AnnotationMapper annotationMapper;
+    @Resource
+    private SingleSlideMapper singleSlideMapper;
+    @Resource
+    private AiForecastService aiForecastService;
+    @Resource
+    private ImageMapper imageMapper;
 
 
-
-	private static Annotation handleSingleJsonElement(JsonNode element, Map<String, Long> pathologicalMap, JsonTask jsonTask,String resolutionX) {
-		if (element.isObject()) {
-			JsonNode node = element.get("id");
-			// node 转换成String
-			String annotationId = node.asText();
-			if (StringUtils.isEmpty(annotationId)) {
-				log.info("annotationId解析失败");
-				return null;
-			}
-			JsonNode node1 = element.get("properties");
-			// 将node1转换成Properties实体类
-			ObjectMapper mapper = new ObjectMapper();
-			Properties properties = null;
-			try {
-				properties = mapper.treeToValue(node1, Properties.class);
-			} catch (JsonProcessingException e) {
-				log.error("Unexpected error occurred: " + e.getMessage(), e);
-			}
-			if (null == properties) {
-				log.info("properties解析失败");
-				return null;
-			}
-			JsonNode geometry = element.get("geometry");
-			// geometry转换成JSONObject
-			JSONObject jsonObject = JSONObject.parseObject(JSONObject.toJSONString(geometry));
-			if (null == jsonObject) {
-				log.info("geometry解析失败");
-				return null;
-			}
-			JsonNode geometry10000 = element.get("geometry10000");
-			// geometry转换成JSONObject
-			JSONObject jsonObject10000 = JSONObject.parseObject(JSONObject.toJSONString(geometry10000));
-			if (null == jsonObject10000) {
-				log.info("geometry10000解析失败");
-				return null;
-			}
-			JsonNode geometry2500 = element.get("geometry2500");
-			// geometry转换成JSONObject
-			JSONObject jsonObject2500 = JSONObject.parseObject(JSONObject.toJSONString(geometry2500));
-			if (null == jsonObject2500) {
-				log.info("geometry2500解析失败");
-				return null;
-			}
-			JsonNode geometry625 = element.get("geometry625");
-			// geometry转换成JSONObject
-			JSONObject jsonObject625 = JSONObject.parseObject(JSONObject.toJSONString(geometry625));
-			if (null == jsonObject625) {
-				log.info("geometry625解析失败");
-				return null;
-			}
-			String labelCode = properties.getLabel_code();
-			if (StringUtils.isEmpty(labelCode)) {
-				log.info("labelCode为空");
-				return null;
-			}
-			String annotationType = properties.getAnnotation_type();
-			if (StringUtils.isEmpty(annotationType)) {
-				log.info("annotationType为空");
-				return null;
-			}
-			Annotation annotation = new Annotation();
-
-			// 查询标签信息
-			Map<String, Indicator> dataIndicators = properties.getData_indicators();
-			if (!CollectionUtil.isEmpty(dataIndicators)) {
-				dataIndicators.forEach((k, v) -> {
-					if (StringUtils.isNotEmpty(v.getUnit())) {
-						double value = v.getValue();
-						BigDecimal bigDecimal = new BigDecimal(resolutionX);
-						BigDecimal bigDecimal1 = new BigDecimal(value);
-						annotation.setArea(bigDecimal1.multiply(bigDecimal).multiply(bigDecimal).multiply(new BigDecimal(0.000001)).setScale(3, RoundingMode.HALF_UP) + "");
-					}
-				});
-			}
-			// 查询标签信息
-			annotation.setPerimeter(properties.getPerimeter());
-			annotation.setCreateBy(0L);
-			annotation.setCreateTime(String.valueOf(new Date()));
-			annotation.setProjectId(0L);
-			annotation.setSlideId(jsonTask.getSlideId());
-			if (null != geometry) {
-				annotation.setContour40000(geometry.toString());
-			}
-			if (null != geometry10000) {
-				annotation.setContour10000(geometry10000.toString());
-			}
-			if (null != geometry2500) {
-				annotation.setContour2500(geometry2500.toString());
-			}
-			if (null != geometry625) {
-				annotation.setContour625(geometry625.toString());
-			}
-			annotation.setId(annotationId);
-			// 拿到categoryId
-			Long categoryId = pathologicalMap.get(labelCode);
-			if (null == categoryId) {
-				log.info("categoryId解析失败");
-				return null;
-			}
-			annotation.setSingleSlideId(jsonTask.getSingleId());
-			annotation.setCategoryId(categoryId);
-			annotation.setAnnotationType(annotationType.toUpperCase());
-			return annotation;
-		} else {
-			log.error("Expected an object, but got a non-object node: " + element);
-			return null;
-		}
-	}
-
-	private static void processObjectNode(ObjectMapper objectMapper, JsonParser jsonParser, List<JsonNode> elementsList) throws IOException {
-		ObjectNode objectNode = objectMapper.readTree(jsonParser);
-		if (objectNode.has("features")) {
-			ArrayNode featuresNode = (ArrayNode) objectNode.get("features");
-			if (featuresNode.isArray()) {
-				Iterator<JsonNode> elementsIterator = featuresNode.elements();
-				while (elementsIterator.hasNext()) {
-					elementsList.add(elementsIterator.next());
-				}
-			} else {
-				log.error("Merged JSON data is not an array of objects as expected at current JSON object.");
-			}
-		} else {
-			log.info("'features' field not found in the current JSON object.");
-		}
-	}
-
-	private static Annotation processJsonElement(JsonNode element, ExecutorService executorService, Map<String, Long> pathologicalMap, JsonTask jsonTask,String resolutionX) {
-
-		try {
-			Future<Annotation> future = executorService.submit(() -> OvariesOviductParserStrategyImpl.handleSingleJsonElement(element, pathologicalMap,jsonTask,resolutionX));
-			Annotation annotation = future.get();
-			future.get(30, TimeUnit.SECONDS);  // 设定超时时间以避免无限等待
-			return annotation;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-
-	@Override
-	public void parseJson(JsonTask jsonTask, JsonFile jsonFileS) {
-        log.info("开始解析卵巢组织");
-        String filePath = jsonFileS.getFileUrl();
-
-        QueryWrapper<SpecialAnnotationRel> wrapper = new QueryWrapper<>();
-        wrapper.eq("special_id", jsonTask.getSpecialId());
-        SpecialAnnotationRel annotationRel = specialAnnotationRelMapper.selectOne(wrapper);
-        Long sequenceNumber = annotationRel.getSequenceNumber();
-        Annotation anno = new Annotation();
-        anno.setSequenceNumber(sequenceNumber);
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonFactory jsonFactory = objectMapper.getFactory();
-        File jsonFile = new File(filePath);
-        QueryWrapper<PathologicalIndicatorCategory> qw = new QueryWrapper<>();
-        // 查询所有未被删除且登录机构相同的数据
-        qw.eq("del_flag", 0).eq("organization_id", jsonTask.getOrganizationId());
-        List<PathologicalIndicatorCategory> list = pathologicalIndicatorCategoryMapper.selectList(qw);
-        Map<String, Long> pathologicalMap = list.stream().collect(Collectors.toMap(PathologicalIndicatorCategory::getStructureId, PathologicalIndicatorCategory::getCategoryId, (entity1, entity2) -> entity1));
-
-        try (FileInputStream fis = new FileInputStream(jsonFile);
-             JsonParser jsonParser = jsonFactory.createParser(fis)) {
-
-            List<JsonNode> elementsList = new ArrayList<>();
-            while (!jsonParser.isClosed()) {
-                JsonToken token = jsonParser.nextToken();
-                if (token == null) {
-                    break;
-                }
-                if (token == JsonToken.START_OBJECT) {
-                    processObjectNode(objectMapper, jsonParser, elementsList);
-                }
-            }
-            if (CollectionUtil.isEmpty(elementsList)) {
-                log.error("No valid JSON data found in the file.");
-                return;
-            }
-            Image image = imageMapper.selectById(jsonTask.getImageId());
-            String resolutionX = image.getResolutionX();
-            if (StringUtils.isEmpty(resolutionX)) {
-                resolutionX = "0.262";
-            }
-            String finalResolutionX = resolutionX;
-            List<Annotation> processedAnnotations;
-            processedAnnotations = elementsList.parallelStream()
-                    .map(element -> {
-                        Annotation annotation = handleSingleJsonElement(element, pathologicalMap, jsonTask, finalResolutionX);
-                        if (!ObjectUtil.isEmpty(annotation)) {
-                            return annotation;
-                        }
-                        return null;
-                    }).filter(Objects::nonNull).collect(Collectors.toList());
-
-            anno.setList(processedAnnotations);
-            log.info("卵巢:{}", processedAnnotations.size());
-            batchProcessAndSave(anno, 1000);
-        } catch (Exception e) {
-            log.error("Unexpected error occurred: " + e.getMessage(), e);
-        }
+    @PostConstruct
+    public void init() {
+        setAiForecastService(aiForecastService);
+        setAnnotationMapper(annotationMapper);
+        setPathologicalIndicatorCategoryMapper(pathologicalIndicatorCategoryMapper);
+        setSingleSlideMapper(singleSlideMapper);
+        setSpecialAnnotationRelMapper(specialAnnotationRelMapper);
+        setImageMapper(imageMapper);
+        log.info("OvariesOviductParserStrategyImpl init");
     }
-
+    
 	@Override
 	public void alculationIndicators(JsonTask jsonTask) {
 		log.info("大鼠卵巢构指标计算开始");
@@ -303,17 +96,8 @@ public class OvariesOviductParserStrategyImpl implements ParserStrategy {
 
 			//黄体面积（全片） 2=C
 			Annotation structureArea = annotationMapper.getStructureArea(annotation1);
-
-			//SingleSlide singleSlide = singleSlideMapper.selectById(jsonTask.getSingleId());
-			//查询切片缩放
-			String resolution = singleSlideMapper.getImageId(jsonTask.getSlideId());
-			BigDecimal resolutions = new BigDecimal("0.262");
-	        if(StringUtils.isNotEmpty(resolution)){
-	            resolutions= new BigDecimal(resolution);
-	        }
 			if (StringUtils.isNotEmpty(structureArea.getArea())) {
 				BigDecimal bigDecimal1 = new BigDecimal(structureArea.getArea());
-				bigDecimal1 = bigDecimal1.multiply(resolutions).multiply(resolutions).multiply(new BigDecimal(0.000001));
 				AiForecast aiForecast1 = new AiForecast();
 				aiForecast1.setQuantitativeIndicators("黄体面积（全片）");
 				aiForecast1.setQuantitativeIndicatorsEn("Corpus luteum area(all)");
@@ -340,32 +124,13 @@ public class OvariesOviductParserStrategyImpl implements ParserStrategy {
 			insertEntity.add(aiForecast2);
 		}
 
-
 		aiForecastService.saveBatch(insertEntity);
 
 	}
 
-	public void batchProcessAndSave(Annotation annotation, int batchSize) {
-
-        List<Annotation> annotations = annotation.getList();
-        if (CollectionUtil.isEmpty(annotations)) {
-            return;
-        }
-        int listSize = annotations.size();
-
-        // 分批处理
-        for (int i = 0; i < listSize; i += batchSize) {
-            int endIndex = Math.min(i + batchSize, listSize);
-            List<Annotation> batch = annotations.subList(i, endIndex);
-            Annotation annotation1 = new Annotation();
-            annotation1.setSequenceNumber(annotation.getSequenceNumber());
-            annotation1.setList(batch);
-            try {
-                annotationMapper.batchSave(annotation1);
-            } catch (Exception e) {
-                // 处理异常，例如记录日志
-                log.error("Error occurred while processing batch: " + e.getMessage(), e);
-            }
-        }
+	
+	@Override
+    public String getAlgorithmCode() {
+        return "Ovaries";
     }
 }
