054f11356bc490fb34ea3f3f76ba5e0546b52fe3
wudi <wud@staitech.cn>
2024-05-17 15:55:50 +0800

    猴舌

--- src/main/java/cn/staitech/fr/service/strategy/json/impl/EsophagusParserStrategyImpl.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/EsophagusParserStrategyImpl.java
@@ -1,6 +1,7 @@
 package cn.staitech.fr.service.strategy.json.impl;
 
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.ObjectUtil;
 import cn.staitech.fr.domain.AiForecast;
 import cn.staitech.fr.domain.JsonTask;
 import cn.staitech.fr.domain.SingleSlide;
@@ -11,12 +12,14 @@ import cn.staitech.fr.service.strategy.json.AbstractCustomParserStrategy;
 import cn.staitech.fr.service.strategy.json.CommonJsonParser;
 import cn.staitech.fr.utils.AreaUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -57,6 +60,10 @@ public class EsophagusParserStrategyImpl extends AbstractCustomParserStrategy {
         //组织轮廓面积
         List<AiForecast> insertEntity = new ArrayList<>();
         SingleSlide singleSlide = singleSlideMapper.selectById(jsonTask.getSingleId());
+        BigDecimal bigDecimal = new BigDecimal(0);
+        if(ObjectUtil.isNotEmpty(singleSlide)&& StringUtils.isNotEmpty(singleSlide.getArea())){
+            bigDecimal = bigDecimal.add(new BigDecimal(singleSlide.getArea()));
+        }
         //面积
         AiForecast aiForecast = new AiForecast();
         aiForecast.setQuantitativeIndicators("食管面积");
@@ -64,7 +71,7 @@ public class EsophagusParserStrategyImpl extends AbstractCustomParserStrategy {
         aiForecast.setUnit("平方毫米");
         aiForecast.setSingleSlideId(jsonTask.getSingleId());
         BigDecimal area = areaUtils.getOrganArea(jsonTask, "10F120");
-        aiForecast.setResults(new BigDecimal(singleSlide.getArea()).subtract(area).toString());
+        aiForecast.setResults(bigDecimal.subtract(area).setScale(3, RoundingMode.HALF_UP).toString());
         aiForecast.setCreateTime(DateUtil.now());
         insertEntity.add(aiForecast);
         aiForecastService.saveBatch(insertEntity);
