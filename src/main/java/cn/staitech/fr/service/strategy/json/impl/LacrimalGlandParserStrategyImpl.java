02ca9dc1da90c3147a261f31d3b06227d81c9f20
wangfeng <wangf@staitech.cn>
2024-05-18 14:26:11 +0800

    泪腺 - OK

--- src/main/java/cn/staitech/fr/service/strategy/json/impl/LacrimalGlandParserStrategyImpl.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/LacrimalGlandParserStrategyImpl.java
@@ -1,5 +1,6 @@
 package cn.staitech.fr.service.strategy.json.impl;
 
+import cn.staitech.fr.constant.CommonConstant;
 import cn.staitech.fr.domain.JsonFile;
 import cn.staitech.fr.domain.JsonTask;
 import cn.staitech.fr.domain.SingleSlide;
@@ -13,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,10 +43,47 @@ public class LacrimalGlandParserStrategyImpl implements ParserStrategy {
     @Override
     public void alculationIndicators(JsonTask jsonTask) {
         Map<String, IndicatorAddIn> indicatorResultsMap = new HashMap<>();
+
+        //        泪腺
+        //
+        //        结构	编码
+        //        导管	16906F
+        //        腺泡细胞核	16906E
+        //        上皮顶部胞质	16906A
+        //        间质	169027
+        //        组织轮廓	169111
+        //        169027.json  16906A.json  16906E.json  16906F.json
+
+        //        算法输出指标	指标代码（仅限本文档）	单位（保留小数点后三位）	备注
+        //        导管面积	A	平方毫米	数据相加输出
+        BigDecimal ductArea = commonJsonParser.getOrganArea(jsonTask, "16906F").getStructureAreaNum();
+        //        腺泡细胞核数量	B	个	无
+        Integer nucleusCount = commonJsonParser.getOrganAreaCount(jsonTask, "16906E");
+        //        腺泡细胞核面积（单个）	C	平方微米	单个数值输出
+        BigDecimal nucleusArea = commonJsonParser.getOrganArea(jsonTask, "16906E").getStructureAreaNum();
+        //        上皮顶部胞质面积	D	平方毫米	数据相加输出
+        BigDecimal epithelialApexCytoplasmArea = commonJsonParser.getOrganArea(jsonTask, "16906A").getStructureAreaNum();
+        //        间质面积	E	平方毫米	数据相加输出
+        BigDecimal mesenchymeArea = commonJsonParser.getOrganArea(jsonTask, "169027").getStructureAreaNum();
+        //        组织轮廓面积	F	平方毫米	无
         SingleSlide singleSlide = singleSlideMapper.selectById(jsonTask.getSingleId());
+
+        //        产品呈现指标	指标代码（仅限本文档）	单位（保留小数点后三位）	English	计算方式	备注
+        //        导管占比	1	无	Duct area%	1=A/F
+        //        腺泡细胞核密度	2	个/平方毫米	Nucleus density of acinus	2=B/(F-E)
+        //        上皮顶部胞质占比	3	无	Epithelial apex cytoplasm area%	3=D/F
+        //        间质占比	4	无	Mesenchyme area%	4=E/F
+        //        腺泡占比	5	无	Acinus area%	5=(F-E)/F
+        //        腺泡细胞核面积（单个）	6	平方微米	Acinar nucleus area (per)	6=C	以95%置信区间和均数±标准差呈现
+        //        泪腺面积	7	平方毫米	Lacrimal gland area	7=F
+
+        indicatorResultsMap.put("导管面积", new IndicatorAddIn("Duct area", ductArea.toString(), "平方毫米", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("腺泡细胞核数量", new IndicatorAddIn("Nucleus counts of acinus", nucleusCount.toString(), "个", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("腺泡细胞核面积（单个）", new IndicatorAddIn("Nucleus area of acinus", nucleusArea.toString(), "平方毫米", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("上皮顶部胞质面积", new IndicatorAddIn("Epithelial apex cytoplasm area", epithelialApexCytoplasmArea.toString(), "平方毫米", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("间质面积", new IndicatorAddIn("Mesenchyme area", mesenchymeArea.toString(), "平方毫米", CommonConstant.NUMBER_1));
         indicatorResultsMap.put("泪腺面积", new IndicatorAddIn("Lacrimal gland area", singleSlide.getArea(), "平方毫米"));
 
         aiForecastService.addAiForecast(jsonTask.getSingleId(), indicatorResultsMap);
     }
-
 }
