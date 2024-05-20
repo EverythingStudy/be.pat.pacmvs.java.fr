9437d65154669d212eda87c72a37cbb457fa8ad0
muguowei <123456789>
2024-05-20 11:17:25 +0800

    算法计算指标

--- src/main/java/cn/staitech/fr/service/strategy/json/impl/PancreasParserStrategyImpl.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/PancreasParserStrategyImpl.java
@@ -1,5 +1,6 @@
 package cn.staitech.fr.service.strategy.json.impl;
 
+import cn.staitech.fr.constant.CommonConstant;
 import cn.staitech.fr.domain.*;
 import cn.staitech.fr.domain.in.IndicatorAddIn;
 import cn.staitech.fr.mapper.*;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,11 +39,63 @@ public class PancreasParserStrategyImpl extends AbstractCustomParserStrategy {
         log.info("PancreasParserStrategyImpl init");
     }
 
+    /**
+     * 上皮细胞核	105075
+     * 酶原颗粒	105076
+     * 胰岛	105077
+     * 胰岛细胞核  105078
+     * 间质	105027
+     * 导管	10506F
+     * 导管细胞核	10507B
+     * 血管	105003
+     * 红细胞	105004
+     * 组织轮廓	105111
+     * 算法输出指标	指标代码（仅限本文档）	单位（保留小数点后三位）	备注
+     * 上皮细胞核数量	A	个	无
+     * 酶原颗粒面积	B	平方毫米	数据相加输出
+     * 胰岛数量	C	个	无
+     * 胰岛面积（单个）	D	103平方微米	单个胰岛面积输出
+     * 胰岛面积（全片）	E	平方毫米	数据相加输出
+     * 胰岛细胞核数量（单个）	F	个	单个胰岛内胰岛细胞核数量输出
+     * 间质面积	G	平方毫米	数据相加输出
+     * 导管数量	H	个	无
+     * 导管面积（单个）	I	103平方微米	单个导管面积输出
+     * 导管面积（全片）	J	平方毫米	数据相加输出
+     * 导管细胞核数量（单个）	K	个	单个导管内导管细胞核数量输出
+     * 血管面积	L	平方毫米	无
+     * 血管内红细胞面积
+     * 	M	平方毫米	数据相加输出
+     * 血管外红细胞面积
+     * 	N	平方毫米	数据相加输出
+     * 组织轮廓面积	O	平方毫米	数据相加输出
+     * 胰岛细胞核数量（全片）	P	个	无
+     * @param jsonTask
+     */
     @Override
     public void alculationIndicators(JsonTask jsonTask) {
         Map<String, IndicatorAddIn> indicatorResultsMap = new HashMap<>();
+        BigDecimal unit = new BigDecimal(1000000);
         SingleSlide singleSlide = singleSlideMapper.selectById(jsonTask.getSingleId());
-        indicatorResultsMap.put("胰腺面积", new IndicatorAddIn("Pancreas area%", singleSlide.getArea(), "平方毫米"));
+        BigDecimal organArea = getOrganArea(jsonTask, "105076",unit).getStructureAreaNum();
+        BigDecimal organArea1 = getOrganArea(jsonTask, "105077",unit).getStructureAreaNum();
+        BigDecimal organArea2 = getOrganArea(jsonTask, "105027").getStructureAreaNum();
+        BigDecimal organArea3 = getOrganArea(jsonTask, "10506F").getStructureAreaNum();
+        BigDecimal organArea4 = getOrganArea(jsonTask, "105003").getStructureAreaNum();
+        Integer count = getOrganAreaCount(jsonTask, "105075");
+        Integer count1 = getOrganAreaCount(jsonTask, "105077");
+        Integer count2 = getOrganAreaCount(jsonTask, "10506F");
+        Integer count3 = getOrganAreaCount(jsonTask, "105078");
+        indicatorResultsMap.put("酶原颗粒面积", new IndicatorAddIn("", organArea.setScale(3, RoundingMode.HALF_UP).toString(), "平方毫米", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("胰岛面积（全片）", new IndicatorAddIn("", organArea1.setScale(3, RoundingMode.HALF_UP).toString(), "平方毫米", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("间质面积", new IndicatorAddIn("", organArea2.setScale(3, RoundingMode.HALF_UP).toString(), "平方毫米", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("导管面积（全片）", new IndicatorAddIn("", organArea3.setScale(3, RoundingMode.HALF_UP).toString(), "平方毫米", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("血管面积", new IndicatorAddIn("", organArea4.setScale(3, RoundingMode.HALF_UP).toString(), "平方毫米", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("上皮细胞核数量", new IndicatorAddIn("", String.valueOf(count), "个", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("胰岛数量", new IndicatorAddIn("", String.valueOf(count1), "个", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("导管数量", new IndicatorAddIn("", String.valueOf(count2), "个", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("胰岛细胞核数量（全片）", new IndicatorAddIn("", String.valueOf(count3), "个", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("组织轮廓面积", new IndicatorAddIn("", singleSlide.getArea(), "平方毫米", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("胰腺面积", new IndicatorAddIn("Pancreas area%", singleSlide.getArea(), "平方毫米", CommonConstant.NUMBER_0));
         aiForecastService.addAiForecast(jsonTask.getSingleId(), indicatorResultsMap);
     }
 
--- src/main/java/cn/staitech/fr/service/strategy/json/impl/ThymusParserStrategyImpl.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/ThymusParserStrategyImpl.java
@@ -1,5 +1,6 @@
 package cn.staitech.fr.service.strategy.json.impl;
 
+import cn.staitech.fr.constant.CommonConstant;
 import cn.staitech.fr.domain.JsonTask;
 import cn.staitech.fr.domain.SingleSlide;
 import cn.staitech.fr.domain.in.IndicatorAddIn;
@@ -9,9 +10,10 @@ import cn.staitech.fr.service.strategy.json.AbstractCustomParserStrategy;
 import cn.staitech.fr.service.strategy.json.CommonJsonParser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -37,11 +39,36 @@ public class ThymusParserStrategyImpl extends AbstractCustomParserStrategy {
         log.info("ThymusParserStrategyImpl init");
     }
 
+
+    /**
+     *     皮质	14403D
+     *     髓质	14403E
+     *     结缔组织	14403F
+     *     红细胞	145004
+     *     组织轮廓	144111
+     *
+     *
+     *     皮质面积	A	平方毫米	数据相加输出
+     *     髓质面积	B	平方毫米	数据相加输出
+     *     髓质内结缔组织面积	C	平方毫米
+     *     髓质外结缔组织面积	D	平方毫米
+     *     红细胞面积	E	平方毫米	数据相加输出
+     *     组织轮廓	F	平方毫米
+     * @param jsonTask
+     */
+
     @Override
     public void alculationIndicators(JsonTask jsonTask) {
-        Map<String, IndicatorAddIn> indicatorResultsMap = new HashMap<>();
         SingleSlide singleSlide = singleSlideMapper.selectById(jsonTask.getSingleId());
-        indicatorResultsMap.put("胸腺面积", new IndicatorAddIn("Thymus Gland area%", singleSlide.getArea(), "平方毫米"));
+        Map<String, IndicatorAddIn> indicatorResultsMap = new HashMap<>();
+        BigDecimal organArea = getOrganArea(jsonTask, "14403D").getStructureAreaNum();
+        BigDecimal organArea2 = getOrganArea(jsonTask, "14403E").getStructureAreaNum();
+        BigDecimal organArea3 = getOrganArea(jsonTask, "145004").getStructureAreaNum();
+        indicatorResultsMap.put("皮质面积", new IndicatorAddIn("", organArea.setScale(3, RoundingMode.HALF_UP).toString(), "平方毫米", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("髓质面积", new IndicatorAddIn("", organArea2.setScale(3, RoundingMode.HALF_UP).toString(), "平方毫米", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("红细胞", new IndicatorAddIn("", organArea3.setScale(3, RoundingMode.HALF_UP).toString(), "平方毫米", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("组织轮廓", new IndicatorAddIn("", singleSlide.getArea(), "平方毫米", CommonConstant.NUMBER_1));
+        indicatorResultsMap.put("胸腺面积", new IndicatorAddIn("Thymus Gland area%", singleSlide.getArea(), "平方毫米",CommonConstant.NUMBER_0));
         aiForecastService.addAiForecast(jsonTask.getSingleId(), indicatorResultsMap);
     }
 
