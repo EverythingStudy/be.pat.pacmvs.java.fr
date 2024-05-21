cf218b1f5e6d3e16dfe4120fd73b59a8c6939fc9
wangfeng <wangf@staitech.cn>
2024-05-21 11:24:42 +0800

    Json解析-调参

--- src/main/java/cn/staitech/fr/service/strategy/json/ParserStrategyFactory.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/ParserStrategyFactory.java
@@ -20,7 +20,7 @@ public class ParserStrategyFactory {
      * key就是beanName(具体策略实现类中@Component的名字)，value就是接口（具体的实现类）
      * Maps是guava下的封装类型，实则是静态的创建了一个HashMap的对象，Maps可以根据key去获取value对象
      */
-    public final Map<String, ParserStrategy> parserStrategyHashMap = Maps.newHashMapWithExpectedSize(50);
+    public final Map<String, ParserStrategy> parserStrategyHashMap = Maps.newHashMapWithExpectedSize(128);
 
     /**
      * 利用构造函数在项目启动的时候将策略实现类注册到 map里
