b35a3009d2100c065117b6b061efc8039b1f052d
耿江涛 <3207883183@qq.com>
2024-05-30 14:01:04 +0800

    集成算法修改

--- src/main/java/cn/staitech/fr/service/strategy/json/impl/LungParserStrategyImpl.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/LungParserStrategyImpl.java
@@ -41,8 +41,8 @@ public class LungParserStrategyImpl extends AbstractCustomParserStrategy {
         // 查询精细轮廓面积
         SingleSlide singleSlide = singleSlideMapper.selectById(jsonTask.getSingleId());
         String accurateArea = singleSlide.getArea();
-
         Integer count = commonJsonParser.getOrganAreaCount(jsonTask, "14C006");
+
 
         //肺泡上皮细胞核数量
         Double density = count / Double.parseDouble(accurateArea);
@@ -51,7 +51,7 @@ public class LungParserStrategyImpl extends AbstractCustomParserStrategy {
         BigDecimal bronchiArea = commonJsonParser.getOrganArea(jsonTask, "14C002").getStructureAreaNum();
 
         // 查询血管面积
-        BigDecimal vesselArea = commonJsonParser.getOrganArea(jsonTask, "14C003").getStructureAreaNum();
+//        BigDecimal vesselArea = commonJsonParser.getOrganArea(jsonTask, "14C003").getStructureAreaNum();
 
         // 查询血管内红细胞面积
 //        BigDecimal intravascularErythrocyteArea = commonJsonParser.getInsideOrOutside(jsonTask,"14C003","14C004",true).getStructureAreaNum();
@@ -61,11 +61,13 @@ public class LungParserStrategyImpl extends AbstractCustomParserStrategy {
 
         Map<String, IndicatorAddIn> indicatorResultsMap = new HashMap<>();
         indicatorResultsMap.put("肺脏面积", new IndicatorAddIn("Lung area", accurateArea, "平方毫米"));
+
+
         indicatorResultsMap.put("肺泡上皮细胞核密度", new IndicatorAddIn("Nucleus density of alveolar epithelial cell", String.valueOf(density), "个/平方毫米"));
 
         indicatorResultsMap.put("支气管面积", new IndicatorAddIn("支气管面积", String.valueOf(bronchiArea), "平方毫米", CommonConstant.NUMBER_1));
-        indicatorResultsMap.put("血管面积", new IndicatorAddIn("血管面积", String.valueOf(vesselArea), "平方毫米", CommonConstant.NUMBER_1));
-        indicatorResultsMap.put("肺泡上皮细胞核数量", new IndicatorAddIn("肺泡上皮细胞核数量", String.valueOf(density), "个", CommonConstant.NUMBER_1));
+//        indicatorResultsMap.put("血管面积", new IndicatorAddIn("血管面积", String.valueOf(vesselArea), "平方毫米", CommonConstant.NUMBER_1));
+//        indicatorResultsMap.put("肺泡上皮细胞核数量", new IndicatorAddIn("肺泡上皮细胞核数量", String.valueOf(density), "个", CommonConstant.NUMBER_1));
 //        indicatorResultsMap.put("血管内红细胞面积", new IndicatorAddIn("血管内红细胞面积", String.valueOf(intravascularErythrocyteArea), "平方毫米", "1"));
 //        indicatorResultsMap.put("血管外红细胞面积", new IndicatorAddIn("血管外红细胞面积", String.valueOf(extravascularErythrocyteArea), "平方毫米", "1"));
         aiForecastService.addAiForecast(jsonTask.getSingleId(), indicatorResultsMap);
