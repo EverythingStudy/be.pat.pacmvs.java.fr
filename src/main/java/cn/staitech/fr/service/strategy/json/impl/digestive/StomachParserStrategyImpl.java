db754b3320e3a16b857b75fa899ddf3e6d8ce614
wudi <wud@staitech.cn>
2024-06-12 11:42:47 +0800

    胃腺胃肌层面积指标去除

--- src/main/java/cn/staitech/fr/service/strategy/json/impl/digestive/StomachParserStrategyImpl.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/digestive/StomachParserStrategyImpl.java
@@ -58,7 +58,7 @@ public class StomachParserStrategyImpl extends AbstractCustomParserStrategy {
         indicatorResultsMap.put("前胃角质层面积", new IndicatorAddIn("Stratum Corneum area", organArea1.setScale(3, RoundingMode.HALF_UP).toString(), "10³平方微米", CommonConstant.NUMBER_1));
         indicatorResultsMap.put("腺胃黏膜上皮+固有层面积", new IndicatorAddIn("Glandular area", organArea2.setScale(3, RoundingMode.HALF_UP).toString(), "平方毫米", CommonConstant.NUMBER_1));
         indicatorResultsMap.put("前胃有核层面积", new IndicatorAddIn("Nucleated cell laye area", organArea3.setScale(3, RoundingMode.HALF_UP).toString(), "10³平方微米", CommonConstant.NUMBER_1));
-        indicatorResultsMap.put("腺胃肌层面积", new IndicatorAddIn("Glandular Muscularis area", organArea4.setScale(3, RoundingMode.HALF_UP).toString(), "平方毫米", CommonConstant.NUMBER_1));
+        //indicatorResultsMap.put("腺胃肌层面积", new IndicatorAddIn("Glandular Muscularis area", organArea4.setScale(3, RoundingMode.HALF_UP).toString(), "平方毫米", CommonConstant.NUMBER_1));
         aiForecastService.addAiForecast(jsonTask.getSingleId(), indicatorResultsMap);
         //aiForecastService.addOutIndicators(jsonTask.getSingleId(), indicatorResultsMap);
     }
