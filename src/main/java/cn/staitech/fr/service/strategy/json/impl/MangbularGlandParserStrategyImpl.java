420832cedfc4ee1346b6f5d33fcaed9a51fdd009
耿江涛 <3207883183@qq.com>
2024-06-19 11:44:13 +0800

    计算指标修改1.3

--- src/main/java/cn/staitech/fr/service/strategy/json/impl/MangbularGlandParserStrategyImpl.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/MangbularGlandParserStrategyImpl.java
@@ -40,6 +40,39 @@ public class MangbularGlandParserStrategyImpl extends AbstractCustomParserStrate
         log.info("MangbularGlandParserStrategyImpl init");
     }
 
+    /**
+     结构	编码
+     颗粒管（红色）	10B125
+     黏液腺细胞核	10B128
+     颗粒管细胞核	10B126
+     有血管壁的血管	10B003
+     红细胞	10B004
+     间质	10B027
+     小叶间导管	10B115
+     组织轮廓	10B111
+     算法输出指标	指标代码（仅限本文档）	单位（保留小数点后三位）	备注
+     颗粒管（红色）数量	A	个	无
+     黏液腺细胞核数量	B	个	无
+     颗粒管细胞核数量（单个）	C	个	无
+     有血管壁的血管面积	D	平方毫米	数据相加输出
+     有血管壁的血管数量	E	个	无
+     红细胞面积	F	平方毫米	数据相加输出
+     颗粒管（红色）面积（单个）	G	103平方微米	无
+     组织轮廓	H	平方毫米	无
+     颗粒管（红色）面积（全片）	I	平方毫米	数据相加输出
+
+     产品呈现指标	指标代码（仅限本文档）	单位（保留小数点后三位）	English	计算方式	备注
+     颗粒管（红色）密度	1	个/平方毫米	Density of granular convoluted tubules (eosinophilic) 	1=A/H
+     黏液腺细胞核密度	2	个/平方毫米	Nucleus density of mucous gland	2=B/H
+     颗粒管细胞核密度(单个颗粒管)	3	个/平方毫米	Nucleus density of granular convoluted tubule (per)	3=C/G	以95%置信区间和均数±标准差呈现
+     血管面积占比	4	%	Vessel area%	4=D/H	运算前注意统一单位；
+     即有血管壁的血管面积
+     红细胞面积占比	5	%	Erythrocyte area%	5=F/H	运算前注意统一单位
+     颌下腺面积	6	平方毫米	Submadibular gland
+     area	6=H
+     颗粒管面积占比（全片）	7	%	Granular convoluted tubules area% (all)	7=I/H
+     */
+
     @Override
     public void alculationIndicators(JsonTask jsonTask) {
         Map<String, IndicatorAddIn> resultsMap = new HashMap<>();
@@ -51,6 +84,7 @@ public class MangbularGlandParserStrategyImpl extends AbstractCustomParserStrate
         Integer organAreaCountE = areaUtils.getOrganAreaCount(jsonTask, "10B003");// E有血管壁的血管数量
         BigDecimal organAreaF = areaUtils.getOrganArea(jsonTask, "10B004");// F红细胞面积
         String slideArea = areaUtils.getFineContourArea(jsonTask.getSingleId()); // H组织轮廓
+        BigDecimal organAreaH = BigDecimal.valueOf(Long.parseLong(slideArea));
         BigDecimal organAreaI = areaUtils.getOrganArea(jsonTask, "10B125");// I颗粒管（红色）面积（全片）
         // todo C颗粒管细胞核数量（单个）10B126
 
@@ -66,12 +100,23 @@ public class MangbularGlandParserStrategyImpl extends AbstractCustomParserStrate
         // 计算指标
         BigDecimal densityResult = getDensityResult(organAreaCountA, slideArea);// A/H
         BigDecimal nucleusResult = getDensityResult(organAreaCountB, slideArea);// B/H
+        // 颗粒管细胞核密度(单个颗粒管) 置信区---
+        // 血管面积占比
+        BigDecimal vesselArea = commonJsonParser.getProportion(organAreaD, organAreaH);
+        // 红细胞面积占比
+        BigDecimal erythrocyteArea = commonJsonParser.getProportion(organAreaF, organAreaH);
+        // 颗粒管面积占比（全片）
+        BigDecimal granularConvolutedTubulesArea = commonJsonParser.getProportion(organAreaI, organAreaH);
+
 
         // 产品呈现指标
         resultsMap.put("颌下腺面积", createNameIndicator("Submadibular gland area", slideArea, SQ_MM));
         resultsMap.put("颗粒管（红色）密度", createNameIndicator("Density of granular convoluted tubules (eosinophilic)", densityResult, SQ_MM_PIECE));
         resultsMap.put("黏液腺细胞核密度", createNameIndicator("Nucleus density of mucous gland", nucleusResult, SQ_MM_PIECE));
-
+//        resultsMap.put("颗粒管细胞核密度(单个颗粒管)", createNameIndicator("Nucleus density of granular convoluted tubule (per)", getDensityResult(organAreaCountC, slideArea), SQ_MM_PIECE));
+        resultsMap.put("血管面积占比", createNameIndicator("Vessel area%", vesselArea, PERCENTAGE));
+        resultsMap.put("红细胞面积占比", createNameIndicator("Erythrocyte area%", erythrocyteArea, PERCENTAGE));
+        resultsMap.put("颗粒管面积占比（全片）", createNameIndicator("Granular convoluted tubules area% (all)", granularConvolutedTubulesArea, PERCENTAGE));
         aiForecastService.addAiForecast(jsonTask.getSingleId(), resultsMap);
     }
 
--- src/main/java/cn/staitech/fr/service/strategy/json/impl/MesentericLymphNodeParserStrategyImpl.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/MesentericLymphNodeParserStrategyImpl.java
@@ -38,6 +38,25 @@ public class MesentericLymphNodeParserStrategyImpl extends AbstractCustomParserS
         log.info("MesentericLymphNodeParserStrategyImpl init");
     }
 
+    /**
+     结构	编码
+     淋巴滤泡	146050
+     生发中心	146051
+     髓质	14603E
+     组织轮廓	146111
+     算法输出指标	指标代码（仅限本文档）	单位（保留3位小数）	备注
+     生发中心数量	A	个
+     生发中心面积（全片）	B	平方毫米	数据相加输出
+     髓质面积	C	平方毫米
+     组织轮廓面积	D	平方毫米
+
+     产品呈现指标	指标代码（仅限本文档）	单位（保留3位小数）	English	计算方式	备注
+     生发中心数量	1	个	 Number of germinal center	1=A
+     生发中心占比	2	%	Germinal center area%	2=B/D
+     髓质占比	3	%	Medulla area%	3=C/D
+     皮质和副皮质占比	4	%	Cortex and paracortex area%	4=（D-C）/D
+     淋巴结面积	5	平方毫米	Lymph node area	5=D
+     */
     @Override
     public void alculationIndicators(JsonTask jsonTask) {
         Map<String, IndicatorAddIn> resultsMap = new HashMap<>();
@@ -47,6 +66,13 @@ public class MesentericLymphNodeParserStrategyImpl extends AbstractCustomParserS
         BigDecimal organAreaB = areaUtils.getOrganArea(jsonTask, "146051");// B生发中心面积（全片）
         BigDecimal organAreaC = areaUtils.getOrganArea(jsonTask, "14603E");// C髓质面积
         String slideArea = areaUtils.getFineContourArea(jsonTask.getSingleId());// D组织轮廓
+        BigDecimal organAreaD = BigDecimal.valueOf(Long.parseLong(slideArea));
+        // 生发中心占比
+        BigDecimal germinalCenterArea = commonJsonParser.getProportion(organAreaB, organAreaD);
+        // 髓质占比
+        BigDecimal medullaArea = commonJsonParser.getProportion(organAreaC, organAreaD);
+        // 皮质和副皮质占比
+        BigDecimal cortexAndParacortexArea = commonJsonParser.getProportionMultiply(organAreaD.subtract(organAreaC), organAreaD);
 
         // 算法输出指标
         resultsMap.put("生发中心面积（全片）", createIndicator(organAreaB, SQ_MM));
@@ -55,7 +81,9 @@ public class MesentericLymphNodeParserStrategyImpl extends AbstractCustomParserS
         // 产品呈现指标
         resultsMap.put("淋巴结面积", createNameIndicator("Submadibular gland area", slideArea, SQ_MM));
         resultsMap.put("生发中心数量", createNameIndicator("Number of germinal center", areaCountA, PIECE));
-
+        resultsMap.put("生发中心占比", createNameIndicator("Germinal center area%", germinalCenterArea, PERCENTAGE));
+        resultsMap.put("髓质占比", createNameIndicator("Medulla area%", medullaArea, PERCENTAGE));
+        resultsMap.put("皮质和副皮质占比", createNameIndicator("Cortex and paracortex area%", cortexAndParacortexArea, PERCENTAGE));
         aiForecastService.addAiForecast(jsonTask.getSingleId(), resultsMap);
     }
 
