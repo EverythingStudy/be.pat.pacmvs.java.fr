71eab57fed232aca939337ad20a2a65fdb516f72
耿江涛 <3207883183@qq.com>
2024-09-11 08:49:27 +0800

    新增Outline实体类及相关服务与控制器，实现吸管功能的统计分析和数据保存逻辑。

---
+++ src/main/java/cn/staitech/fr/mapper/OutlineMapper.java
@@ -0,0 +1,16 @@
+package cn.staitech.fr.mapper;
+
+
+import cn.staitech.fr.domain.Outline;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * (Outline)表数据库访问层
+ *
+ * @author wangfeng
+ * @since 2024-01-04 10:55:03
+ */
+public interface OutlineMapper extends BaseMapper<Outline> {
+
+}
+
