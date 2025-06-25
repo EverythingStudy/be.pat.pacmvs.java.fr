d525503e551c1754102697462981d68a053cc32d
muguowei <123456789>
2025-06-25 15:12:57 +0800

    refactor(fail): 优化项目配置权限的错误提示信息
    - 修改了项目状态为"进行中"时的错误提示信息，允许配置用户
    - 更新了项目状态为"完成"时的错误提示信息，使其更加简洁
    -改进了项目状态为"暂停"时的错误提示信息，明确了权限限制

--- src/main/java/cn/staitech/fr/service/impl/SlideServiceImpl.java
+++ src/main/java/cn/staitech/fr/service/impl/SlideServiceImpl.java
@@ -238,16 +238,19 @@ public class SlideServiceImpl extends ServiceImpl<SlideMapper, Slide> implements
 		Integer status = project.getStatus();
 
 		if (status == Constants.STATUS_RUNNING) {
-			return R.fail("项目状态为“进行中”时，不能配置项目基础信息和切片");
+//			return R.fail("项目状态为“进行中”时，不能配置项目基础信息和切片");
+			return R.fail("项目进行中，除配置用户外不可修改其他配置");
 		}
 
 		if (status == Constants.STATUS_COMPLETED) {
-			return R.fail("项目状态为“完成”时，任何用户不能再配置项目任何信息");
+//			return R.fail("项目状态为“完成”时，任何用户不能再配置项目任何信息");
+			return R.fail("项目已完成，不可修改配置");
 		}
 
 		if (status == Constants.STATUS_PAUSED
 				&& !(SecurityUtils.getUserId() == project.getPrincipal() || SecurityUtils.isOrgAdmin())) {
-			return R.fail("项目状态为“暂停”时，机构管理员和项目负责人可以配置项目基础信息");
+//			return R.fail("项目状态为“暂停”时，机构管理员和项目负责人可以配置项目基础信息");
+			return R.fail("您没有该项目的配置权限，请联系该项目负责人或机构管理员");
 		}
 
 		return null; // 表示通过校验
