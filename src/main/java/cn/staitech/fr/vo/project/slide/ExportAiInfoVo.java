4158b1e3d9359de74ba85d25b6ff4de94799d2a8
jiazx <18311369014@163.com>
2025-08-13 10:24:09 +0800

    excel表头注解配置

--- src/main/java/cn/staitech/fr/vo/project/slide/ExportAiInfoVo.java
+++ src/main/java/cn/staitech/fr/vo/project/slide/ExportAiInfoVo.java
@@ -1,5 +1,6 @@
 package cn.staitech.fr.vo.project.slide;
 
+import cn.staitech.common.core.annotation.Excel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -7,20 +8,36 @@ import lombok.Data;
 public class ExportAiInfoVo {
 
     @ApiModelProperty(value = "专题号")
+    @Excel(name = "专题号")
     private String topicName;
+
     @ApiModelProperty(value = "项目名称")
+    @Excel(name = "项目名称")
     private String specialName;
+
     @ApiModelProperty(value = "图像名称")
+    @Excel(name = "图像名称")
     private String imageName;
+
     @ApiModelProperty(value = "脏器名称")
+    @Excel(name = "脏器名称")
     private String organName;
+
     @ApiModelProperty(value = "量化指标")
+    @Excel(name = "量化指标")
     private String quantitativeIndicators;
+
     @ApiModelProperty(value = "数值")
+    @Excel(name = "数值")
     private String results;
+
     @ApiModelProperty(value = "单位")
+    @Excel(name = "单位")
     private String unit;
+
+
     @ApiModelProperty(value = "对照组数值分布区间")
+    @Excel(name = "对照组数值分布区间")
     private String forecastRange;
 
 }
