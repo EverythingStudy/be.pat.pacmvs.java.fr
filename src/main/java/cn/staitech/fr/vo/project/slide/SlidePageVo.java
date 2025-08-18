43e8007329f69f0c427bdd86e225d59dce5ce83f
叶向阳 <765655780@qq.com>
2025-08-18 15:40:13 +0800

    V2.6.1切片列表返回对象

--- src/main/java/cn/staitech/fr/vo/project/slide/SlidePageVo.java
+++ src/main/java/cn/staitech/fr/vo/project/slide/SlidePageVo.java
@@ -3,6 +3,7 @@ package cn.staitech.fr.vo.project.slide;
 import java.util.Date;
 import java.util.List;
 
+import cn.staitech.fr.domain.Organ;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -58,10 +59,15 @@ public class SlidePageVo {
     @ApiModelProperty(value = "阅片状态集合：0-未阅片；1-已阅片-V2.6.1")
     private Integer viewStatus;
     /**
-     * AI分析状态
+     * AI分析状态：0-未分析、1-脏器识别中、2-脏器识别异常、3-脏器识别完成、4-结构未分析、5-结构分析中、6-结构分析完成、7-结构分析失败
      */
-    @ApiModelProperty(value = "AI分析状态：0-未分析、1-脏器识别中、2-脏器识别异常、3-结构未分析、4-结构分析中、5-结构分析完成、6-结构分析失败-V2.6.1")
+    @ApiModelProperty(value = "AI分析状态：0-未分析、1-脏器识别中、2-脏器识别异常、4-结构未分析、5-结构分析中、6-结构分析完成、7-结构分析失败（列表阅片模式下：非0、1、2状态请使用organStatusVos；矩阵阅片模式下：全状态可使用）-V2.6.1")
     private Integer aiStatus;
+    /**
+     * 脏器信息状态集合
+     */
+    @ApiModelProperty(value = "脏器信息状态集合（列表阅片模式下：aiStatus非0、1、2状态使用）-V2.6.1")
+    List<OrganStatusVo> organStatusVos;
     /**
      * 描述
      */
