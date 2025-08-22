d6f32c89e21875a1a8fb8d648679518d411b058e
叶向阳 <765655780@qq.com>
2025-08-22 16:02:22 +0800

    文件名称修改

---
+++ sql/V2.6.1-PostgreSQL.sql
@@ -0,0 +1,5 @@
+-- postgresql
+ALTER TABLE "fr_annotation" ADD COLUMN "contour_type" int2 DEFAULT 0;
+COMMENT ON COLUMN "fr_annotation"."contour_type" IS '标注类型：默认 0  1 粗轮廓 2 精细轮廓';
+ALTER TABLE "fr_annotation_del" ADD COLUMN "contour_type" int2 DEFAULT 0;
+COMMENT ON COLUMN "fr_annotation_del"."contour_type" IS '标注类型：默认 0  1 粗轮廓 2 精细轮廓';
