1dbe4ff7425eb9ce2513412bacd5be834e20ba5f
chenly <759499199@qq.com>
2025-08-29 15:24:54 +0800

    AI指标信息下载

--- src/main/java/cn/staitech/fr/domain/out/ExprotAiExcelVO.java
+++ src/main/java/cn/staitech/fr/domain/out/ExprotAiExcelVO.java
@@ -19,18 +19,23 @@ public class ExprotAiExcelVO {
     @ExcelProperty(index = 2, value = "图像名称")
     private String imageName;
     @ApiModelProperty(value = "脏器名称")
+    @ExcelProperty(index = 3, value = "脏器名称")
     private String organName;
 
     @ApiModelProperty(value = "量化指标")
+    @ExcelProperty(index = 4, value = "量化指标")
     private String quantitativeIndicators;
 
     @ApiModelProperty(value = "预测结果")
+    @ExcelProperty(index = 5, value = "预测结果")
     private String results;
 
     @ApiModelProperty(value = "单位")
+    @ExcelProperty(index = 6, value = "单位")
     private String unit;
 
     @ApiModelProperty(name = "normalDistribution", value = "正态分布95%")
+    @ExcelProperty(index = 7, value = "正态分布95%")
     private String normalDistribution;
 
 }
