b909b6e94bdd1061a6e8bed75ae67ad6363ebc34
chenly <759499199@qq.com>
2025-09-26 15:51:16 +0800

    优化结构解析入库

--- src/main/java/cn/staitech/fr/service/strategy/json/CommonJsonParser.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/CommonJsonParser.java
@@ -134,20 +134,20 @@ public class CommonJsonParser {
             annotation.setProjectId(0L);
 
             if (null != geometry) {
-                annotation.setContour40000(JSONObject.toJSONString(convertGeoJsonToPGGeometry(geometry.toString())));
+                annotation.setContour40000(JSONObject.toJSONString(geometry.toString()));
             }
             if (null != geometry10000) {
-                annotation.setContour10000(JSONObject.toJSONString(convertGeoJsonToPGGeometry(geometry10000.toString())));
+                annotation.setContour10000(JSONObject.toJSONString(geometry10000.toString()));
             }
             if (null != geometry2500) {
-                annotation.setContour2500(JSONObject.toJSONString(convertGeoJsonToPGGeometry(geometry2500.toString())));
+                annotation.setContour2500(JSONObject.toJSONString(geometry2500.toString()));
             }
             if (null != geometry625) {
-                annotation.setContour625(JSONObject.toJSONString(convertGeoJsonToPGGeometry(geometry625.toString())));
+                annotation.setContour625(JSONObject.toJSONString(geometry625.toString()));
             }
             //
             if (null != geometry0) {
-                annotation.setContour5000(JSONObject.toJSONString(convertGeoJsonToPGGeometry(geometry0.toString())));
+                annotation.setContour5000(JSONObject.toJSONString(geometry0.toString()));
             }
             annotation.setId(annotationId);
             // 拿到categoryId
@@ -169,6 +169,7 @@ public class CommonJsonParser {
             return null;
         }
     }
+
     /**
      * 将GeoJSON转换为PostGIS几何对象
      */
