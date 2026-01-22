b6a47cb9b6e0af69899a10a1d21f4785bb661587
muguowei <mgwmgwmgw@126.com>
2026-01-22 11:35:20 +0800

    fix(vo): 修正ImageBatchIdsVO继承类型
    - 将ImageBatchIdsVO的泛型参数从ImageBatchIdsVO改为ImageLogDetail
    - 解决了继承关系中的类型不匹配问题

--- src/main/java/cn/staitech/fr/vo/image/ImageBatchIdsVO.java
+++ src/main/java/cn/staitech/fr/vo/image/ImageBatchIdsVO.java
@@ -11,7 +11,7 @@ import java.util.List;
  * @author wangf
  */
 @Data
-public class ImageBatchIdsVO extends LogAuditBaseReq<ImageBatchIdsVO> {
+public class ImageBatchIdsVO extends LogAuditBaseReq<ImageLogDetail> {
     /**
      * 图像id
      */
