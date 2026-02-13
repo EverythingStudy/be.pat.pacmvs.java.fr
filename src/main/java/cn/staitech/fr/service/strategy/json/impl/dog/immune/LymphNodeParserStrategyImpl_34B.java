5b21b22272667e014fa414b3697c6a56833a4344
wangtc <wangtc@st.com>
2026-02-13 09:51:29 +0800

    犬类指标公式，免疫系统+感觉系统

---
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/dog/immune/LymphNodeParserStrategyImpl_34B.java
@@ -0,0 +1,82 @@
+package cn.staitech.fr.service.strategy.json.impl.dog.immune;
+
+import cn.staitech.fr.constant.CommonConstant;
+import cn.staitech.fr.domain.JsonTask;
+import cn.staitech.fr.domain.in.IndicatorAddIn;
+import cn.staitech.fr.mapper.SingleSlideMapper;
+import cn.staitech.fr.service.AiForecastService;
+import cn.staitech.fr.service.strategy.json.AbstractCustomParserStrategy;
+import cn.staitech.fr.service.strategy.json.CommonJsonCheck;
+import cn.staitech.fr.service.strategy.json.CommonJsonParser;
+import cn.staitech.fr.utils.DecimalUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>犬类-免疫系统-淋巴结</p>
+ * <a href="https://docs.staitech.cn/i/nodes/7gNKMlbrq3WXA1dnW00H4Xv5p6Ad9nL1">文档</a>
+ *
+ * @author wangtc
+ */
+@Component
+@Slf4j
+public class LymphNodeParserStrategyImpl_34B extends AbstractCustomParserStrategy {
+    private final SingleSlideMapper singleSlideMapper;
+    private final AiForecastService aiForecastService;
+
+    public LymphNodeParserStrategyImpl_34B(
+            CommonJsonParser commonJsonParser,
+            CommonJsonCheck commonJsonCheck,
+            SingleSlideMapper singleSlideMapper,
+            AiForecastService aiForecastService
+    ) {
+        setCommonJsonParser(commonJsonParser);
+        setCommonJsonCheck(commonJsonCheck);
+        this.singleSlideMapper = singleSlideMapper;
+        this.aiForecastService = aiForecastService;
+    }
+
+    @Override
+    public String getAlgorithmCode() {
+        return "Lymph_node_3";
+    }
+
+    @Override
+    public void alculationIndicators(JsonTask jsonTask) {
+        Map<String, IndicatorAddIn> map = new HashMap<>();
+        // 生发中心数量 A
+        Integer countA = getOrganAreaCount(jsonTask, "34B051");
+        map.put("生发中心数量", new IndicatorAddIn("", countA.toString(), PIECE, CommonConstant.NUMBER_0, "34B054,34B051"));
+        // 生发中心面积 B
+        BigDecimal areaB = getOrganArea(jsonTask, "34B051").getStructureAreaNum();
+        map.put("生发中心面积", new IndicatorAddIn("", DecimalUtils.setScale3(areaB), SQ_MM, CommonConstant.NUMBER_0, "34B054,34B051"));
+        // 皮质+副皮质区面积 C
+        BigDecimal areaC = getOrganArea(jsonTask, "34B052").getStructureAreaNum();
+        map.put("皮质+副皮质区面积", new IndicatorAddIn("", DecimalUtils.setScale3(areaC), SQ_MM, CommonConstant.NUMBER_0, "34B052"));
+        // 髓质面积 D
+        BigDecimal areaD = getOrganArea(jsonTask, "34B03E").getStructureAreaNum();
+        map.put("髓质面积", new IndicatorAddIn("", DecimalUtils.setScale3(areaD), SQ_MM, CommonConstant.NUMBER_0, "34B03E"));
+        // 组织轮廓面积 E
+        BigDecimal areaE = new BigDecimal(singleSlideMapper.selectById(jsonTask.getSingleId()).getArea());
+//        map.put("组织轮廓面积", new IndicatorAddIn("", DecimalUtils.setScale3(areaE), SQ_MM, CommonConstant.NUMBER_0, "34B111"));
+        // 生发中心数量 1=A
+        map.put("生发中心数量", new IndicatorAddIn("Number of germinal center", countA.toString(), PIECE, CommonConstant.NUMBER_1, "34B054,34B051"));
+        // 生发中心面积占比 2=B/E
+        BigDecimal proportion2 = getProportion(areaB, areaE);
+        map.put("生发中心面积占比", new IndicatorAddIn("Germinal center area%", DecimalUtils.setScale3(proportion2), PERCENTAGE, CommonConstant.NUMBER_1, "34B054,34B051,34B111"));
+        // 皮质+副皮质区占比 3=C/E
+        BigDecimal proportion3 = getProportion(areaC, areaE);
+        map.put("皮质+副皮质区占比", new IndicatorAddIn("Cortex and paracortex area%", DecimalUtils.setScale3(proportion3), PERCENTAGE, CommonConstant.NUMBER_1, "34B052,34B111"));
+        // 髓质占比 4=D/E
+        BigDecimal proportion4 = getProportion(areaD, areaE);
+        map.put("髓质占比", new IndicatorAddIn("Medulla area%", DecimalUtils.setScale3(proportion4), PERCENTAGE, CommonConstant.NUMBER_1, "34B03E,34B111"));
+        // 淋巴结面积 5=E
+        map.put("淋巴结面积", new IndicatorAddIn("Lymph node area", DecimalUtils.setScale3(areaE), SQ_MM, CommonConstant.NUMBER_1, "34B111"));
+        // 保存结果
+        aiForecastService.addAiForecast(jsonTask.getSingleId(), map);
+    }
+}
---
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/dog/sensory/LacrimalGlandParserStrategyImpl_369.java
@@ -0,0 +1,88 @@
+package cn.staitech.fr.service.strategy.json.impl.dog.sensory;
+
+import cn.staitech.fr.constant.CommonConstant;
+import cn.staitech.fr.domain.Annotation;
+import cn.staitech.fr.domain.JsonTask;
+import cn.staitech.fr.domain.in.IndicatorAddIn;
+import cn.staitech.fr.mapper.SingleSlideMapper;
+import cn.staitech.fr.service.AiForecastService;
+import cn.staitech.fr.service.strategy.json.AbstractCustomParserStrategy;
+import cn.staitech.fr.service.strategy.json.CommonJsonCheck;
+import cn.staitech.fr.service.strategy.json.CommonJsonParser;
+import cn.staitech.fr.utils.AreaUtils;
+import cn.staitech.fr.utils.DecimalUtils;
+import cn.staitech.fr.utils.MathUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * <p>犬类-特殊感觉系统-泪腺</p>
+ * <a href="https://docs.staitech.cn/i/nodes/7gNKMlbrq3WXA1LOYv2C4Xv5p6Ad9nL1">文档</a>
+ *
+ * @author wangtc
+ */
+@Component
+@Slf4j
+public class LacrimalGlandParserStrategyImpl_369 extends AbstractCustomParserStrategy {
+    private final SingleSlideMapper singleSlideMapper;
+    private final AiForecastService aiForecastService;
+    private final AreaUtils areaUtils;
+
+    public LacrimalGlandParserStrategyImpl_369(
+            CommonJsonParser commonJsonParser,
+            CommonJsonCheck commonJsonCheck,
+            SingleSlideMapper singleSlideMapper,
+            AiForecastService aiForecastService,
+            AreaUtils areaUtils
+    ) {
+        setCommonJsonParser(commonJsonParser);
+        setCommonJsonCheck(commonJsonCheck);
+        this.singleSlideMapper = singleSlideMapper;
+        this.aiForecastService = aiForecastService;
+        this.areaUtils = areaUtils;
+    }
+
+    @Override
+    public String getAlgorithmCode() {
+        return "Lacrimal_gland_3";
+    }
+
+    @Override
+    public void alculationIndicators(JsonTask jsonTask) {
+        Map<String, IndicatorAddIn> map = new HashMap<>();
+        // 腺泡面积 A
+        BigDecimal areaA = getOrganArea(jsonTask, "36906D").getStructureAreaNum();
+        map.put("腺泡面积", new IndicatorAddIn("", DecimalUtils.setScale3(areaA), SQ_MM, CommonConstant.NUMBER_0, "36906D"));
+        // 腺泡细胞核数量 B
+        Integer countB = getOrganAreaCount(jsonTask, "36906E");
+        map.put("腺泡细胞核数量", new IndicatorAddIn("", countB.toString(), PIECE, CommonConstant.NUMBER_0, "36906E"));
+        // 腺泡细胞核面积（单个） C -> 产品呈现指标
+        Annotation ann = new Annotation();
+        ann.setAreaName("腺泡细胞核面积（单个）");
+        ann.setAreaUnit(SQ_UM);
+        getCommonJsonParser().putSingleAnnotationDynamicData(jsonTask, "36906E", ann, 2);
+        // 组织轮廓面积 D
+        BigDecimal areaD = new BigDecimal(singleSlideMapper.selectById(jsonTask.getSingleId()).getArea());
+//        map.put("组织轮廓面积", new IndicatorAddIn("", DecimalUtils.setScale3(areaD), SQ_MM, CommonConstant.NUMBER_0, "369111"));
+        // 腺泡面积占比 1=A/D
+        BigDecimal proportion1 = getProportion(areaA, areaD);
+        map.put("腺泡面积占比", new IndicatorAddIn("Acinus area%", DecimalUtils.setScale3(proportion1), PERCENTAGE, CommonConstant.NUMBER_1, "36906D,369111"));
+        // 腺泡细胞核密度 2=B/A
+        BigDecimal density2 = bigDecimalDivideCheck(BigDecimal.valueOf(countB), areaA);
+        map.put("腺泡细胞核密度", new IndicatorAddIn("Nucleus density of acinus", DecimalUtils.setScale3(density2), SQ_MM_PIECE, CommonConstant.NUMBER_1, "36906E"));
+        // 腺泡细胞核面积（单个） 3=C
+        List<Annotation> listC = getCommonJsonParser().getStructureContourList(jsonTask, "36906E");
+        List<BigDecimal> statC = listC.stream().map(annC -> new BigDecimal(areaUtils.convertToMicrometer(annC.getStructureAreaNum().toString()))).collect(Collectors.toList());
+        map.put("腺泡细胞核面积（单个）", new IndicatorAddIn("Acinar nucleus area (per)", MathUtils.getConfidenceInterval(statC), SQ_UM, CommonConstant.NUMBER_1, "36906E"));
+        // 泪腺面积 4=D
+        map.put("泪腺面积", new IndicatorAddIn("Lacrimal gland area", DecimalUtils.setScale3(areaD), SQ_MM, CommonConstant.NUMBER_1, "369111"));
+        // 保存结果
+        aiForecastService.addAiForecast(jsonTask.getSingleId(), map);
+    }
+}
