16a41998f6f71af0f818c9d2e0f8696c1a0c965b
叶向阳 <765655780@qq.com>
2026-02-28 17:09:59 +0800

    卵巢

--- src/main/java/cn/staitech/fr/service/strategy/json/impl/dog/reproduction/Ovaries_3ParserStrategyImpl.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/dog/reproduction/Ovaries_3ParserStrategyImpl.java
@@ -1,6 +1,5 @@
 package cn.staitech.fr.service.strategy.json.impl.dog.reproduction;
 
-import cn.staitech.fr.domain.Annotation;
 import cn.staitech.fr.domain.JsonTask;
 import cn.staitech.fr.domain.SingleSlide;
 import cn.staitech.fr.domain.in.IndicatorAddIn;
@@ -10,7 +9,6 @@ import cn.staitech.fr.service.strategy.json.AbstractCustomParserStrategy;
 import cn.staitech.fr.service.strategy.json.CommonJsonCheck;
 import cn.staitech.fr.service.strategy.json.CommonJsonParser;
 import cn.staitech.fr.utils.AreaUtils;
-import cn.staitech.fr.utils.MathUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -18,9 +16,7 @@ import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -72,7 +68,7 @@ public class Ovaries_3ParserStrategyImpl extends AbstractCustomParserStrategy {
         organAreaF = organAreaF.multiply(new BigDecimal(1000000)).setScale(3, RoundingMode.HALF_UP);
         // G：组织轮廓面积mm2
         SingleSlide singleSlide = this.singleSlideMapper.selectById(jsonTask.getSingleId());
-        String organAreaG = singleSlide.getArea();
+        BigDecimal organAreaG = new BigDecimal(singleSlide.getArea());
 
         // 指标存放
        /* resultsMap.put("卵泡数量", createIndicator(countA, PIECE, "3240CB"));
@@ -94,22 +90,22 @@ public class Ovaries_3ParserStrategyImpl extends AbstractCustomParserStrategy {
         resultsMap.put("卵泡面积（单个）", createNameIndicator("Follicle area", spermAreaPer, MULTIPLIED_SQ_UM_THOUSAND, "3240CB"));
 
         // 3=A/G：卵泡密度
-        BigDecimal ag3 = this.commonJsonParser.bigDecimalDivideCheck(BigDecimal.valueOf(countA), new BigDecimal(organAreaG));
+        BigDecimal ag3 = this.commonJsonParser.bigDecimalDivideCheck(BigDecimal.valueOf(countA), organAreaG);
         resultsMap.put("卵泡密度", createNameIndicator("Follicle density", ag3, SQ_MM_PIECE, this.areaUtils.getStructureIds("3240CB", "324111")));
         // 4=B/G：卵泡面积占比
-        BigDecimal bg4 = this.commonJsonParser.bigDecimalDivideCheck(organAreaB, new BigDecimal(organAreaG));
+        BigDecimal bg4 = this.commonJsonParser.bigDecimalDivideCheck(organAreaB, organAreaG);
         resultsMap.put("卵泡面积占比", createNameIndicator("Follicle area%", bg4, PERCENTAGE, this.areaUtils.getStructureIds("3240CB", "324111")));*/
         // 5=D/G：血管面积占比
-        BigDecimal dg5 = this.commonJsonParser.bigDecimalDivideCheck(organAreaD, new BigDecimal(organAreaG).multiply(new BigDecimal(1000000)));
+        BigDecimal dg5 = getProportion(organAreaD, organAreaG.multiply(new BigDecimal(1000000)));
         resultsMap.put("血管面积占比", createNameIndicator("Vessel area%", dg5, PERCENTAGE, this.areaUtils.getStructureIds("324003", "324111")));
         // 6=E/G：血管外红细胞面积占比
-        BigDecimal eg6 = this.commonJsonParser.bigDecimalDivideCheck(organAreaE, new BigDecimal(organAreaG).multiply(new BigDecimal(1000000)));
+        BigDecimal eg6 = getProportion(organAreaE, organAreaG.multiply(new BigDecimal(1000000)));
         resultsMap.put("血管外红细胞面积占比", createNameIndicator("Extravascular Erythrocyte area%", eg6, PERCENTAGE, this.areaUtils.getStructureIds("324003", "324004", "324111")));
         // 7=F/G：血管内红细胞面积占比
-        BigDecimal fg7 = this.commonJsonParser.bigDecimalDivideCheck(organAreaF, new BigDecimal(organAreaG).multiply(new BigDecimal(1000000)));
+        BigDecimal fg7 = getProportion(organAreaF, organAreaG.multiply(new BigDecimal(1000000)));
         resultsMap.put("血管外红细胞面积占比", createNameIndicator("Intravascular Erythrocyte area%", fg7, PERCENTAGE, this.areaUtils.getStructureIds("324003", "324004", "324111")));
         // 8=G：卵巢面积
-        resultsMap.put("卵巢面积", createNameIndicator("Ovary area", new BigDecimal(organAreaG), SQ_MM, "324111"));
+        resultsMap.put("卵巢面积", createNameIndicator("Ovary area", organAreaG, SQ_MM, "324111"));
 
         this.aiForecastService.addAiForecast(jsonTask.getSingleId(), resultsMap);
     }
