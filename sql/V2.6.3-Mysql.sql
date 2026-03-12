a587c3890dbbd62a052fa8234c4dbd63ed30385c
叶向阳 <765655780@qq.com>
2026-03-12 11:01:05 +0800

    algorithm_method

--- sql/V2.6.3-Mysql.sql
+++ sql/V2.6.3-Mysql.sql
@@ -3,5 +3,7 @@ ALTER TABLE fr_production
 DROP INDEX `idx_special_id`,
 ADD INDEX `idx_special_id`(`special_id` ASC) USING BTREE;
 
-ALTER TABLE `pathmedics`.`tb_species_wax_code_template`
+ALTER TABLE tb_species_wax_code_template
+    ADD COLUMN `algorithm_method` varchar(255) NULL COMMENT '对应算法接口脏器编码：只记录不同的' AFTER `abbreviation`;
+ALTER TABLE fr_production
     ADD COLUMN `algorithm_method` varchar(255) NULL COMMENT '对应算法接口脏器编码：只记录不同的' AFTER `abbreviation`;
