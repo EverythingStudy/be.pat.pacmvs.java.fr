1b6983e8b9f901fb940154f69ed3c3992a3554f7
wanglibei <wanglb@staitech.cn>
2026-03-18 18:48:49 +0800

    1、增加日志信息 2、增加任务相关切片线程池数量统计信息

--- src/main/java/cn/staitech/fr/config/DynamicDataPool.java
+++ src/main/java/cn/staitech/fr/config/DynamicDataPool.java
@@ -75,7 +75,7 @@ public class DynamicDataPool {
     static class CustomRejectedExecutionHandler implements RejectedExecutionHandler {
         @Override
         public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
-            String msg = String.format("识别任务被拒绝！[活跃:%d/最大:%d] [队列:%d/%d] 任务:%s",
+            String msg = String.format("识别任务被拒绝！[当前活跃线程数:%d/最大允许线程数:%d] [队列中任务数:%d/队列剩余容量:%d] 任务:%s",
                     executor.getActiveCount(), 
                     executor.getMaximumPoolSize(),
                     executor.getQueue().size(), 
