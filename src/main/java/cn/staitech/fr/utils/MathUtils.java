1a535f3f46d3ec8a2bdece1bc06078ba600bb11b
叶向阳 <765655780@qq.com>
2026-03-19 13:08:32 +0800

    均值±标准差；中间95%数据分布区间 不算95了

--- src/main/java/cn/staitech/fr/utils/MathUtils.java
+++ src/main/java/cn/staitech/fr/utils/MathUtils.java
@@ -219,8 +219,11 @@ public class MathUtils {
 //            }
 //            //正态分布(上限)
 //            BigDecimal add2 = bigDecimal.add(new BigDecimal(1.96).multiply(sqrt)).setScale(3, RoundingMode.UP);
-            String middle95Percent = getFirstAndLastOfMiddle95Percent(dataList.stream().sorted().map(e -> e.setScale(3, RoundingMode.UP)).collect(Collectors.toList()), dataList.size());
-            return bigDecimal + "±" + sqrt + ";" + middle95Percent;
+
+            return bigDecimal + "±" + sqrt;
+
+          /*  String middle95Percent = getFirstAndLastOfMiddle95Percent(dataList.stream().sorted().map(e -> e.setScale(3, RoundingMode.UP)).collect(Collectors.toList()), dataList.size());
+            return bigDecimal + "±" + sqrt + ";" + middle95Percent;*/
         } else {
             return 0 + "±" + 0 + ";" + 0 + "-" + 0;
         }
