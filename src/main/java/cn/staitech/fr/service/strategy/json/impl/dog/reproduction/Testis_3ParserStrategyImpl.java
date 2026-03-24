eb2d82aeb08bf3b603f48ba330fa43ca31ee12b0
叶向阳 <765655780@qq.com>
2026-03-24 16:16:27 +0800

    间质细胞核数量

--- src/main/java/cn/staitech/fr/service/strategy/json/impl/dog/reproduction/Testis_3ParserStrategyImpl.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/dog/reproduction/Testis_3ParserStrategyImpl.java
@@ -65,7 +65,7 @@ public class Testis_3ParserStrategyImpl extends AbstractCustomParserStrategy {
         annotationC.setPerimeterUnit(UM);
         this.commonJsonParser.putSingleAnnotationDynamicData(jsonTask, "32E0FA", annotationC, 1);
         // D：生精小管数量
-        Integer countD = organ.getCount();
+        Integer countD = this.commonJsonParser.getOrganAreaCount(jsonTask, "32E0FA");
         // E：生精小管内腔面积（单个）×103 μm2
         Annotation annotationE = new Annotation();
         annotationE.setAreaName("生精小管内腔面积（单个）");
@@ -82,7 +82,7 @@ public class Testis_3ParserStrategyImpl extends AbstractCustomParserStrategy {
         annotationG.setCountUnit(PIECE);
         this.commonJsonParser.putAnnotationDynamicData(jsonTask, "32E0FA", "32E0FD", annotationF, 1, true);
         // H：间质细胞核数量
-        Integer countH = this.commonJsonParser.getOrganArea(jsonTask, "32E0FE").getCount();
+        Integer countH = this.commonJsonParser.getOrganAreaCount(jsonTask, "32E0FE");
         // I：组织轮廓面积
         SingleSlide singleSlide = this.singleSlideMapper.selectById(jsonTask.getSingleId());
         BigDecimal organAreaI = new BigDecimal(singleSlide.getArea());
