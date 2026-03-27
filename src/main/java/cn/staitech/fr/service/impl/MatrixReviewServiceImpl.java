c1000dab0c783c70fa7b3c9160c818373d4babfe
jiazx <18311369014@163.com>
2026-03-27 16:15:41 +0800

    指标计算导出

--- src/main/java/cn/staitech/fr/service/impl/MatrixReviewServiceImpl.java
+++ src/main/java/cn/staitech/fr/service/impl/MatrixReviewServiceImpl.java
@@ -9,14 +9,17 @@ import cn.staitech.fr.domain.dto.ExportAiDTO;
 import cn.staitech.fr.domain.out.ExportAiListVO;
 import cn.staitech.fr.domain.out.ExprotAiExcelEnVO;
 import cn.staitech.fr.domain.out.ExprotAiExcelVO;
+import cn.staitech.fr.enmu.SpeciesTypeEnum;
 import cn.staitech.fr.mapper.AiForecastMapper;
 import cn.staitech.fr.mapper.ProjectMapper;
 import cn.staitech.fr.mapper.SingleSlideMapper;
 import cn.staitech.fr.mapper.SlideMapper;
 import cn.staitech.fr.service.MatrixReviewService;
+import cn.staitech.fr.service.SlideService;
 import cn.staitech.fr.utils.ExportPdfUtils;
 import cn.staitech.fr.utils.LanguageUtils;
 import cn.staitech.fr.utils.MathUtils;
+import cn.staitech.fr.vo.AiForecastVo;
 import cn.staitech.fr.vo.project.SlideDownLoadReq;
 import cn.staitech.fr.vo.project.slide.SlidePageVo;
 import com.alibaba.excel.EasyExcel;
@@ -34,10 +37,9 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 import java.util.stream.Collectors;
 
 /**
@@ -58,12 +60,15 @@ public class MatrixReviewServiceImpl implements MatrixReviewService {
 
     @Resource
     private AiForecastMapper aiForecastMapper;
-
+    @Resource
+    private ExecutorService executorService;
 
     @Value("${waxPath}")
     private String waxPath;
     @Resource
     private HttpServletResponse response;
+    @Autowired
+    private SlideService slideService;
 
     @Override
     public void algorithmDownload(SlideDownLoadReq req) throws Exception {
@@ -118,28 +123,132 @@ public class MatrixReviewServiceImpl implements MatrixReviewService {
                     exportAiExcelVO.setTopicName(exportVO.getTopicName());
                     exportAiExcelVO.setImageName(exportVO.getImageName());
                     exportAiExcelVO.setOrganName(exportVO.getOrganName());
-                    if (null != exportAiListVO.getNormalDistribution() && null != exportAiListVO.getResults() && !"数据量过少,无统计学意义".equals(exportAiListVO.getNormalDistribution())) {
-                        String[] s = exportAiListVO.getNormalDistribution().split("-");
-                        if (!"详情见单个标注轮廓详情弹窗！".equals(exportAiListVO.getResults()) && exportAiListVO.getResults().split(";").length == 1) {
-                            try {
-                                String result = exportAiListVO.getResults().contains("±")
-                                        ? exportAiListVO.getResults().split("±")[0]
-                                        : (exportAiListVO.getResults().contains(":") ? exportAiListVO.getResults().split(":")[0] : exportAiListVO.getResults());
-                                boolean inRange = Range.between(new BigDecimal(s[0]), new BigDecimal(s[1])).contains(new BigDecimal(result));
-                                exportAiExcelVO.setLowerBound(s[0]);
-                                exportAiExcelVO.setUpperBound(s[1]);
-                                if (!inRange) {
-                                    exportAiExcelVO.setAbnormalValue("T");
+                    if(special.getSpeciesId().equals(String.valueOf(SpeciesTypeEnum.RAT.getCode()))) {
+                        if (null != exportAiListVO.getNormalDistribution() && null != exportAiListVO.getResults() && !"数据量过少,无统计学意义".equals(exportAiListVO.getNormalDistribution())) {
+                            String[] s = exportAiListVO.getNormalDistribution().split("-");
+                            if (!"详情见单个标注轮廓详情弹窗！".equals(exportAiListVO.getResults()) && exportAiListVO.getResults().split(";").length == 1) {
+                                try {
+                                    String result = exportAiListVO.getResults().contains("±")
+                                            ? exportAiListVO.getResults().split("±")[0]
+                                            : (exportAiListVO.getResults().contains(":") ? exportAiListVO.getResults().split(":")[0] : exportAiListVO.getResults());
+                                    boolean inRange = Range.between(new BigDecimal(s[0]), new BigDecimal(s[1])).contains(new BigDecimal(result));
+                                    exportAiExcelVO.setLowerBound(s[0]);
+                                    exportAiExcelVO.setUpperBound(s[1]);
+                                    if (!inRange) {
+                                        exportAiExcelVO.setAbnormalValue("T");
+                                    }
+                                } catch (Exception e) {
+                                    log.error("数据转换异常{},{}", exportAiListVO.getNormalDistribution(), exportAiListVO.getResults());
                                 }
-                            } catch (Exception e) {
-                                log.error("数据转换异常{},{}",  exportAiListVO.getNormalDistribution(), exportAiListVO.getResults());
+                            } else {
+                                exportAiExcelVO.setAbnormalValue("");
                             }
                         } else {
-                            exportAiExcelVO.setAbnormalValue("");
+                            exportAiExcelVO.setLowerBound(exportAiListVO.getNormalDistribution());
+                            exportAiExcelVO.setUpperBound(exportAiListVO.getNormalDistribution());
                         }
                     } else {
-                        exportAiExcelVO.setLowerBound(exportAiListVO.getNormalDistribution());
-                        exportAiExcelVO.setUpperBound(exportAiListVO.getNormalDistribution());
+                        if (exportAiListVO.getResults().contains("±")) {
+                            List<AiForecastVo> forecastVo = singleSlideMapper.getReferenceScopeCopyV2(aiForecast.getQuantitativeIndicators(), categorys.get(id), special.getProjectId(), special.getControlGroup());
+                            for (AiForecastVo aiForecastVo : forecastVo) {
+                                //首先去掉所有数值为“0±0” 0±0;0-0 的切片
+                                if (aiForecastVo.getResults().contains("0±0")) {
+                                    aiForecastVo.setIsDelete("1");
+                                } else {
+                                    //计算每条指标的加减标准差
+                                    String[] result = aiForecastVo.getResults().split("±");
+                                    BigDecimal meanAddStandardDeviation = new BigDecimal(String.valueOf(result[0])).add(new BigDecimal(result[1]));
+                                    BigDecimal meanSubStandardDeviation = new BigDecimal(String.valueOf(result[0])).subtract(new BigDecimal(result[1]));
+                                    aiForecastVo.setMeanAddStandardDeviation(meanAddStandardDeviation);
+                                    aiForecastVo.setMeanSubStandardDeviation(meanSubStandardDeviation);
+                                }
+                            }
+                            //找出最大 最小的标准差 如果存在多个相同的话只剔除第一个
+                            slideService.markMinMax(forecastVo);
+                            List<AiForecastVo> aiForecastVos = forecastVo.stream().filter(e -> !"1".equals(e.getIsDelete())).collect(Collectors.toList());
+
+                            if (aiForecastVos.size() < 5) {
+                                exportAiExcelVO.setNormalDistribution("数据量过少,无统计学意义");
+                                exportAiExcelVO.setLowerBound("数据量过少,无统计学意义");
+                                exportAiExcelVO.setUpperBound("数据量过少,无统计学意义");
+                            } else {
+
+                                List<String> fileUrls = aiForecastVos.stream().map(AiForecastVo::getFileUrl).filter(StringUtils::isNotBlank).collect(Collectors.toList());
+                                if (CollectionUtils.isNotEmpty(fileUrls)) {
+                                    // 累加文件数量
+                                    int totalCount = fileUrls.stream()
+                                            .filter(url -> url.endsWith(".txt")) // 基础过滤
+                                            .map(url -> {
+                                                int index1 = url.lastIndexOf("-");
+                                                int index2 = url.indexOf(".txt");
+                                                return Integer.parseInt(url.substring(index1 + 1, index2));
+                                            })
+                                            .mapToInt(Integer::intValue)
+                                            .sum();
+
+                                    log.info("读取文件时间开始");
+                                    long currentTimeMillis = System.currentTimeMillis();
+                                    List<BigDecimal> bigDecimalList_ = new ArrayList<>(totalCount);
+                                    // 2. 并行处理每个文件
+                                    List<CompletableFuture<List<BigDecimal>>> futures = fileUrls.stream().map(path -> CompletableFuture.supplyAsync(() -> {
+                                        try {
+                                            return slideService.readFileSingle(path);
+                                        } catch (Exception e) {
+                                            log.error("读取文件失败: {}", path, e);
+                                            return Collections.<BigDecimal>emptyList(); // 空集合
+                                        }
+                                    }, executorService)).collect(Collectors.toList());
+
+                                    // 3. 等待所有任务完成并合并结果
+                                    for (CompletableFuture<List<BigDecimal>> future : futures) {
+                                        bigDecimalList_.addAll(future.join());
+                                    }
+                                    long endTime = System.currentTimeMillis();
+                                    log.info("读取文件时间结束:{} 秒", (endTime - currentTimeMillis)/1000.0);
+                                    String confidenceInterval = MathUtils.getConfidenceIntervalCopy(bigDecimalList_);
+                                    exportAiExcelVO.setNormalDistribution(confidenceInterval);
+
+                                    String s = confidenceInterval.split("±")[0];
+                                    String v = confidenceInterval.split("±")[1];
+                                    BigDecimal up = new BigDecimal(String.valueOf(s)).add(new BigDecimal(v)); //上界
+                                    BigDecimal down = new BigDecimal(String.valueOf(s)).subtract(new BigDecimal(v));//下界
+
+                                    //计算每条指标的加减标准差
+                                    String[] result = aiForecast.getResults().split("±");
+                                    BigDecimal meanAdd = new BigDecimal(String.valueOf(result[0])).add(new BigDecimal(result[1]));
+                                    BigDecimal meanSub = new BigDecimal(String.valueOf(result[0])).subtract(new BigDecimal(result[1]));
+
+                                    //如果该切片的“均值-标准差”小于对照组数值分布区间的“均值-标准差”数值，或该切片的“均值+标准差”大于对照组数值分布区间的“均值+标准差”数值，即判定该切片此指标数值异常，标红提示
+                                    if (meanSub.compareTo(down) < 0 ) {
+                                        exportAiExcelVO.setAbnormalValue("异常，均值-标准差小于对照组下界");
+                                    } else if(meanAdd.compareTo(up) > 0) {
+                                        exportAiExcelVO.setAbnormalValue("异常，均值+标准差大于对照组上界");
+                                    }
+                                }
+                            }
+                        } else {
+                            List<BigDecimal> dataList_ = singleSlideMapper.getReferenceScopeCopy(aiForecast.getQuantitativeIndicators(), categorys.get(id), special.getProjectId(), special.getControlGroup());
+                            Integer count = singleSlideMapper.getCategoryIdCountByGroupCode(categorys.get(id), special.getProjectId(), special.getControlGroup());
+                            exportAiExcelVO.setNormalDistribution(MathUtils.getFirstAndLastOfMiddle95Percent(dataList_, count));
+
+
+                            if (null != exportAiExcelVO.getNormalDistribution() && null != aiForecast.getResults() && !"数据量过少,无统计学意义".equals(exportAiExcelVO.getNormalDistribution())) {
+                                String[] s = exportAiExcelVO.getNormalDistribution().split("-");
+                                if (!"详情见单个标注轮廓详情弹窗！".equals(aiForecast.getResults())) {
+                                    try {
+                                        String result = aiForecast.getResults();
+                                        boolean inRange = Range.between(new BigDecimal(s[0]), new BigDecimal(s[1])).contains(new BigDecimal(result));
+                                        if (!inRange) {
+                                            exportAiExcelVO.setAbnormalValue("T");
+                                        }
+                                    } catch (Exception e) {
+                                        log.error("数据转换异常{},{}", exportAiListVO.getNormalDistribution(), exportAiListVO.getResults());
+                                    }
+                                } else {
+
+                                }
+                            }
+                        }
                     }
                     collect.add(exportAiExcelVO);
                 }
