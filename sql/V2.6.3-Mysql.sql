ab62faaa4290c30d3ecb4a87f9fd0c1113418d0b
叶向阳 <765655780@qq.com>
2026-03-31 10:17:11 +0800

    2w队列

--- sql/V2.6.3-Mysql.sql
+++ sql/V2.6.3-Mysql.sql
@@ -7,5 +7,5 @@ ALTER TABLE tb_species_wax_code_template
     ADD COLUMN `algorithm_method` varchar(255) NULL COMMENT '对应算法接口脏器编码：只记录不同的' AFTER `abbreviation`;
 ALTER TABLE fr_production
     ADD COLUMN `algorithm_method` varchar(255) NULL COMMENT '对应算法接口脏器编码：只记录不同的' AFTER `abbreviation`;
-ALTER TABLE `pacmvs`.`fr_ai_forecast`
+ALTER TABLE fr_ai_forecast
     ADD COLUMN `file_url` varchar(255) NULL COMMENT '计算均值±方差的原始数据，存放到文件' AFTER `structure_ids`;
