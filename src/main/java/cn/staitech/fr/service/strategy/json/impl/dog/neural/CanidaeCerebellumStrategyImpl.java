7c0c1c44c13783c2a265a5d050a18c6077f25878
叶向阳 <765655780@qq.com>
2026-04-02 14:33:45 +0800

    小脑

--- src/main/java/cn/staitech/fr/service/strategy/json/impl/dog/neural/CanidaeCerebellumStrategyImpl.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/dog/neural/CanidaeCerebellumStrategyImpl.java
@@ -19,7 +19,6 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -67,35 +66,36 @@ public class CanidaeCerebellumStrategyImpl extends AbstractCustomParserStrategy 
         Map<String, IndicatorAddIn> map = new LinkedHashMap<>();
         String specialId = "3";
         // A 颗粒细胞层＋浦肯野细胞层	mm2	无
-        BigDecimal choroidOPlexusAreaAnnotation = commonJsonParser.getOrganArea(jsonTask, specialId+"3E0A5").getStructureAreaNum();
+        BigDecimal bigDecimalA = commonJsonParser.getOrganArea(jsonTask, specialId + "3E0A5").getStructureAreaNum();
         // B 血管外红细胞面积	103 μm2	无 (查询血管外红细胞面积)
-        BigDecimal extravascularErythrocyteArea = commonJsonParser.getInsideOrOutside(jsonTask, specialId+"3E003", specialId+"3E004", false).getStructureAreaNum();
+        BigDecimal bigDecimalB = commonJsonParser.getInsideOrOutside(jsonTask, specialId + "3E003", specialId + "3E004", false).getStructureAreaNum();
         // C 血管内红细胞面积	mm2	无 (查询血管内红细胞面积)
-        BigDecimal intravascularErythrocyteArea = commonJsonParser.getInsideOrOutside(jsonTask, specialId+"3E003", specialId+"3E004", true).getStructureAreaNum();
+        BigDecimal bigDecimalC = commonJsonParser.getInsideOrOutside(jsonTask, specialId + "3E003", specialId + "3E004", true).getStructureAreaNum();
         // D:精细轮廓总面积（大鼠大脑）- 平方毫米
         String accurateArea = singleSlideMapper.selectById(jsonTask.getSingleId()).getArea();
-        BigDecimal accurateAreaBigDecimal = new BigDecimal(accurateArea);
+        BigDecimal bigDecimalD = new BigDecimal(accurateArea);
 
         // 算法输出指标 -------------------------------------------------------------
         // A
-        map.put("颗粒细胞层＋浦肯野细胞层面积", new IndicatorAddIn("Granulocyte and Purkinje cell layer area", DecimalUtils.setScale3(choroidOPlexusAreaAnnotation), SQ_MM, CommonConstant.NUMBER_1, specialId+"3E0A5"));
+        map.put("颗粒细胞层＋浦肯野细胞层面积", createIndicator(bigDecimalA, SQ_MM, "33E0A5"));
         // B
-       map.put("血管外红细胞面积", new IndicatorAddIn("Extravascular Erythrocyte area", areaUtils.convertToSquareMicrometer(extravascularErythrocyteArea.toString()), SQ_UM_THOUSAND, CommonConstant.NUMBER_1, specialId+"3E003,"+specialId+"3E004"));
+        map.put("血管外红细胞面积", createIndicator(bigDecimalB.multiply(new BigDecimal(1000)), MULTIPLIED_SQ_UM_THOUSAND, "33E003,33E004"));
         // C
-        map.put("血管内红细胞面积", new IndicatorAddIn("Intravascular Erythrocyte area", areaUtils.convertToSquareMicrometer(intravascularErythrocyteArea.toString()), SQ_MM, CommonConstant.NUMBER_1, specialId+"3E003,"+specialId+"3E004"));
+        map.put("血管内红细胞面积", createIndicator(bigDecimalC, SQ_MM, "33E003,33E004"));
 
         // 产品呈现指标 -------------------------------------------------------------
-        if (accurateAreaBigDecimal.compareTo(BigDecimal.ZERO) != 0) {
-            // 1 脉络丛面积占比	%	Granulocyte and Purkinje cell layer area %	1=A/D	无
-            map.put("颗粒细胞层和浦肯野细胞层面积占比", new IndicatorAddIn("Granulocyte and Purkinje cell layer area %", getProportion(choroidOPlexusAreaAnnotation, accurateAreaBigDecimal).toString(), PERCENTAGE, areaUtils.getStructureIds(specialId+"3E0A5", specialId+"3E111")));
-            // 2 血管外红细胞面积占比	%	Extravascular Erythrocyte area%	2=B/D	无
-            map.put("血管外红细胞面积占比", new IndicatorAddIn("Extravascular Erythrocyte area%", getProportion(extravascularErythrocyteArea, accurateAreaBigDecimal).toString(), PERCENTAGE, specialId+"3E003,"+specialId+"3E004,"+specialId+"3E111"));
-            // 3 血管内红细胞面积占比%	Intravascular Erythrocyte area%	3=C/D	无
-            map.put("血管内红细胞面积占比", new IndicatorAddIn("Intravascular Erythrocyte area%", getProportion(intravascularErythrocyteArea, accurateAreaBigDecimal).toString(), PERCENTAGE, specialId+"3E003,"+specialId+"3E004,"+specialId+"3E111"));
-        }
+        // 1 脉络丛面积占比	%	Granulocyte and Purkinje cell layer area %	1=A/D	无
+        BigDecimal ad1 = getProportion(bigDecimalA, bigDecimalD);
+        map.put("颗粒细胞层和浦肯野细胞层面积占比", createNameIndicator("Granulocyte and Purkinje cell layer area %", ad1, PERCENTAGE, "33E0A5,33E111"));
+        // 2 血管外红细胞面积占比	%	Extravascular Erythrocyte area%	2=B/D	无
+        BigDecimal bd2 = getProportion(bigDecimalB, bigDecimalD);
+        map.put("血管外红细胞面积占比", createNameIndicator("Extravascular Erythrocyte area%", bd2, PERCENTAGE, "33E003,33E004,33E111"));
+        // 3 血管内红细胞面积占比%	Intravascular Erythrocyte area%	3=C/D	无
+        BigDecimal cd3 = getProportion(bigDecimalC, bigDecimalD);
+        map.put("血管内红细胞面积占比", createNameIndicator("Intravascular Erythrocyte area%", cd3, PERCENTAGE, "33E003,33E004,33E111"));
 
         // D 大脑面积	4	平方毫米	Cerebellum and Brainstem area	4=D	无
-        map.put("小脑面积", new IndicatorAddIn("Cerebellum and Brainstem area", DecimalUtils.setScale3(accurateAreaBigDecimal), CommonConstant.SQUARE_MILLIMETRE, specialId+"32111"));
+        map.put("小脑面积", createNameIndicator("Cerebellum and Brainstem area", bigDecimalD, SQ_MM, "33E111"));
         aiForecastService.addAiForecast(jsonTask.getSingleId(), map);
     }
 
