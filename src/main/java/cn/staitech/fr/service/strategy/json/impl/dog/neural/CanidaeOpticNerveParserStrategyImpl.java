428d59ca12ab4cfd9588cf83b3cb4ee9c1350c0d
叶向阳 <765655780@qq.com>
2026-04-02 13:17:42 +0800

    视神经-33F-OE

--- src/main/java/cn/staitech/fr/service/strategy/json/impl/dog/neural/CanidaeOpticNerveParserStrategyImpl.java
+++ src/main/java/cn/staitech/fr/service/strategy/json/impl/dog/neural/CanidaeOpticNerveParserStrategyImpl.java
@@ -15,17 +15,16 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 
-* @ClassName: D65EyeAndOpticNerveParserStrategyImpl
-* @Description-d:眼球(视神经)
-* @author wanglibei
-* @date 2025年7月21日
-* @version V1.0
+ *
+ * @author wanglibei
+ * @version V1.0
+ * @ClassName: D65EyeAndOpticNerveParserStrategyImpl
+ * @Description-d:眼球(视神经)
+ * @date 2025年7月21日
  */
 @Slf4j
 @Service("Optic_nerve_3")
@@ -39,6 +38,7 @@ public class CanidaeOpticNerveParserStrategyImpl extends AbstractCustomParserStr
     private CommonJsonCheck commonJsonCheck;
     @Autowired
     private AreaUtils areaUtils;
+
     @PostConstruct
     public void init() {
         setCommonJsonParser(commonJsonParser);
@@ -65,30 +65,31 @@ public class CanidaeOpticNerveParserStrategyImpl extends AbstractCustomParserStr
 
     @Override
     public void alculationIndicators(JsonTask jsonTask) {
-    	/**
-    	 * 视神经
-	     * 结构	编码
-	     * 神经纤维束	13F0BB
-	     * 神经外膜结缔组织	13F0BA
-	     * 算法输出指标	指标代码（仅限本文档）	单位（保留小数点后3位）	备注
-	     * 神经纤维束面积	A	103平方微米	相加输出
-	     * 神经外膜结缔组织面积	B	平方毫米	无
-	     * <p>
-	     * 产品呈现指标	指标代码（仅限本文档）	单位（保留小数点后3位）	English	计算方式	备注
-	     * 神经纤维束面积	1	平方毫米	Nerve fiber bundles area	1=A	无
-	     * 神经外膜面积	2	103平方微米	Epineurium area	2=B-A	即神经外膜结缔组织面积
-    	 */
-        String specialId = "3";
+        /**
+         * 视神经
+         * 结构	编码
+         * 神经纤维束	13F0BB
+         * 神经外膜结缔组织	13F0BA
+         * 算法输出指标	指标代码（仅限本文档）	单位（保留小数点后3位）	备注
+         * 神经纤维束面积	A	103平方微米	相加输出
+         * 神经外膜结缔组织面积	B	平方毫米	无
+         * <p>
+         * 产品呈现指标	指标代码（仅限本文档）	单位（保留小数点后3位）	English	计算方式	备注
+         * 神经纤维束面积	1	平方毫米	Nerve fiber bundles area	1=A	无
+         * 神经外膜面积	2	103平方微米	Epineurium area	2=B-A	即神经外膜结缔组织面积
+         */
         Map<String, IndicatorAddIn> indicatorResultsMap = new HashMap<>();
-        BigDecimal organArea3 = getOrganArea(jsonTask, specialId+"3F0BB").getStructureAreaNum();
-        BigDecimal organArea4 = getOrganArea(jsonTask, specialId+"3F0BA").getStructureAreaNum();
+        BigDecimal organAreaA = getOrganArea(jsonTask, "33F0BB").getStructureAreaNum();
+        BigDecimal organAreaB = getOrganArea(jsonTask, "33F0BA").getStructureAreaNum();
         //indicatorResultsMap.put("神经纤维束面积", new IndicatorAddIn("", organArea3.setScale(3, RoundingMode.HALF_UP).toString(), CommonConstant.SQUARE_MICROMETER, CommonConstant.NUMBER_1));
-        indicatorResultsMap.put("神经束膜内缘内面积", new IndicatorAddIn("Nerve fiber bundles area", organArea3.setScale(3, RoundingMode.HALF_UP).toString(),SQ_MM, CommonConstant.NUMBER_0));
-        indicatorResultsMap.put("神经外膜结缔组织面积", new IndicatorAddIn("", organArea4.setScale(3, RoundingMode.HALF_UP).toString(),SQ_MM, CommonConstant.NUMBER_1));
+
+        indicatorResultsMap.put("神经外膜结缔组织面积", createIndicator(organAreaB, SQ_MM, "33F0BA"));
+
+        indicatorResultsMap.put("神经束膜内缘内面积", createNameIndicator("Nerve fiber bundles area", organAreaA, SQ_MM, "33F0BB"));
 
 //        BigDecimal A = getOrganArea(jsonTask, "13F0BB",new BigDecimal(1000)).getStructureAreaNum();
 //        BigDecimal B = getOrganArea(jsonTask, "13F0BA",new BigDecimal(1000)).getStructureAreaNum();
-        indicatorResultsMap.put("结缔组织面积", new IndicatorAddIn("Connective tissue area", organArea4.subtract(organArea3).toString(), CommonConstant.SQUARE_MILLIMETRE, CommonConstant.NUMBER_0));
+        indicatorResultsMap.put("结缔组织面积", createNameIndicator("Connective tissue area", organAreaB.subtract(organAreaA), CommonConstant.SQUARE_MILLIMETRE, "33F0BB,33F0BA"));
 
 
         aiForecastService.addAiForecast(jsonTask.getSingleId(), indicatorResultsMap);
