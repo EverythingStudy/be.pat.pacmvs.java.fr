b12f34d29abee2575a78d2b8746cce46606bead6
叶向阳 <765655780@qq.com>
2026-04-08 15:16:39 +0800

    小鼠脏器标签

---
+++ sql/V2.6.3.1_tag-Mysql.sql
@@ -0,0 +1,101 @@
+-- 初始化脏器标签（57条）
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'00','Others','其他','AA','rgb(0, 0, 0)','#000000',1,1,1,0,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'01','Adrenal glands','肾上腺','AG','rgb(255, 0, 0)','#FF0000',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'02','Harderian glands','哈德氏腺','HG','rgb(0, 255, 0)','#00FF00',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'05','Pancreas','胰腺','PA','rgb(0, 0, 255)','#0000FF',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'06','Pituitary','垂体','PI','rgb(255, 255, 0)','#FFFF00',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'07','Thyroid gland','甲状腺','TG','rgb(255, 0, 255)','#FF00FF',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'08','Parathyroid','甲状旁腺','PD','rgb(0, 255, 255)','#00FFFF',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'09','Salivary glands, parotid','唾液腺，腮腺','PG','rgb(0, 114, 189)','#0072BD',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'0A','Salivary glands, sublingual','唾液腺，舌下腺','SD','rgb(217, 83, 25)','#D95319',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'0B','Salivary glands, mandibular','唾液腺，颌下腺','MD','rgb(237, 177, 32)','#EDB120',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'0D','Tongue','舌','TO','rgb(126, 47, 142)','#7E2F8E',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'0E','Larynx','喉','LA','rgb(119, 172, 48)','#77AC30',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'0F','Esophagus','食管','ES','rgb(77, 190, 238)','#4DBEEE',1,1,1,1,1);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'10','Stomach (glandular and non-glandular)','胃（腺胃和非腺胃）','SM','rgb(162, 20, 47)','#A2142F',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'12','Liver','肝脏','LI','rgb(0, 128, 0)','#008000',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'14','Cecum','肠，盲肠','CE','rgb(0, 0, 128)','#000080',1,1,1,0,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'15','Colon','肠，结肠','CO','rgb(128, 128, 0)','#808000',1,1,1,0,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'16','Rectum','肠，直肠','RE','rgb(128, 0, 128)','#800080',1,1,1,0,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'17','Ileum (with Peyer’s Patch)','肠，回肠（含派氏结）','IL','rgb(0, 128, 128)','#008080',1,1,1,0,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'18','Jejunum','肠，空肠','JE','rgb(128, 0, 255)','#8000FF',1,1,1,0,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'19','Duodenum','肠，十二指肠','DU','rgb(0, 128, 255)','#0080FF',1,1,1,0,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'1B','Kidneys','肾脏','KI','rgb(255, 128, 128)','#FF8080',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'1E','Urinary bladder','膀胱','UB','rgb(128, 128, 255)','#8080FF',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'26','Uterus','子宫','UT','rgb(255, 200, 255)','#FFC8FF',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'27','Cervix','子宫颈','CX','rgb(200, 255, 255)','#C8FFFF',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'28','Vagina','阴道','VA','rgb(255, 0, 0)','#FF0000',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'2B','Coagulating glands','凝固腺','CG','rgb(0, 255, 0)','#00FF00',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'2C','Prostate','前列腺','PR','rgb(0, 0, 255)','#0000FF',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'2D','Seminal vesicles','精囊腺','SV','rgb(255, 255, 0)','#FFFF00',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'2E','Testis','睾丸','TE','rgb(255, 0, 255)','#FF00FF',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'2F','Epididymides','附睾','EP','rgb(0, 255, 255)','#00FFFF',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'32','Brain','大脑','BR1_BR2','rgb(0, 114, 189)','#0072BD',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'39','Spinal cord (cervical, thoracic, lumbar)','脊髓（颈、胸、腰）','SC','rgb(217, 83, 25)','#D95319',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'3F','Optic nerve','视神经','OE','rgb(119, 172, 48)','#77AC30',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'40','Nerve, sciatic','神经，坐骨','SE','rgb(77, 190, 238)','#4DBEEE',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'44','Thymus','胸腺','TH','rgb(162, 20, 47)','#A2142F',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'45','Spleen','脾脏','SP','rgb(0, 128, 0)','#008000',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'46','Lymph node, mesenteric','淋巴结，肠系膜','MN','rgb(0, 0, 128)','#000080',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'47','Lymph node, inguinal','淋巴结，腹股沟','IE','rgb(128, 128, 0)','#808000',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'48','Lymph node, mandibular','淋巴结，下颌','ML','rgb(128, 0, 128)','#800080',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'4C','Lung with bronchi','肺（含支气管）','LU','rgb(0, 128, 128)','#008080',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'4D','Trachea','气管','TR','rgb(255, 128, 0)','#FF8000',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'4E','Bone with bone marrow, sternum','骨和骨髓，胸骨','SN','rgb(128, 0, 255)','#8000FF',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'50','Bone with bone marrow, femur','骨和骨髓，股骨','FE','rgb(145, 200, 200)','#91C8C8',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'5C','Muscle','骨骼肌','MU','rgb(255, 200, 200)','#FFC8C8',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'5D','Aorta','主动脉','AO','rgb(200, 255, 200)','#C8FFC8',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'5E','Heart','心脏','HE','rgb(200, 200, 255)','#C8C8FF',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'5F','Eye','眼睛','EY','rgb(255, 255, 200)','#FFFFC8',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'69','Lacrimal glands','泪腺','LG','rgb(255, 200, 255)','#FFC8FF',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'77','Gross lesions observed at necropsy','大体损伤','GL','rgb(128, 128, 255)','#8080FF',1,1,1,0,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'78','Injection site','注射局部','IS','rgb(255, 0, 128)','#FF0080',1,1,1,0,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'7A','Skin (close to mammary) with mammary gland','皮肤（乳腺附近）及乳腺','IN_MG','rgb(128, 128, 255)','#C8FFC8',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'7C','Ovaries with oviducts','卵巢与输卵管','OS_OT','rgb(0, 128, 255)','#C8C8FF',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'7D','Cerebellum with brain stem','小脑与脑干','CM_BS','rgb(128, 0, 255)','#EDB120',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'7F','Thyroid glands with parathyroids','甲状腺与甲状旁腺','TG_PD','rgb(255, 0, 128)','#0080FF',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'82','Small Intestine','小肠','SITT','rgb(255, 0, 128)','#FF0080',1,1,1,1,0);
+INSERT INTO tb_organ_tag (species_id, organ_tag_code, organ_en, organ_name, abbreviation, rgb, chromatic_value, organization_id, create_by, update_by, organ_recognition, structured_analysis) VALUES ( "2",'83','Large Intestine','大肠','LITT','rgb(255, 200, 200)','#FFC8C8',1,1,1,1,0);
+
+-- 同步数据到脏器表
+INSERT INTO tb_organ (organ_code, name, name_en, species_id, organization_id) SELECT organ_tag_code, organ_name, organ_en, species_id, organization_id FROM tb_organ_tag WHERE species_id = '2' AND organization_id = 1;
+
+-- 插入tb_organ中不存在于tb_structure_tag_set的记录
+INSERT INTO tb_structure_tag_set (structure_tag_set_name, structure_tag_set_name_en, species_id, organ_code, organization_id, create_by,update_by)
+SELECT a.name, a.name_en, a.species_id, a.organ_code, a.organization_id, 1, 1
+FROM tb_organ a
+WHERE a.species_id = '2' AND a.organization_id = 1;
+
+-- 结构初始化（6条）
+INSERT INTO tb_structure (species_id,organ_code,structure_id,name,name_en,type,organization_id,structure_size) VALUES ('2','0F','20F12E','角质层','Stratum Corneum','RO',1,1);
+INSERT INTO tb_structure (species_id,organ_code,structure_id,name,name_en,type,organization_id,structure_size) VALUES ('2','0F','20F12F','颗粒层+棘层+基底细胞曾','Nucleated Cell Layer','RO',1,1);
+INSERT INTO tb_structure (species_id,organ_code,structure_id,name,name_en,type,organization_id,structure_size) VALUES ('2','0F','20F00C','肌层','Muscularis','RO',1,1);
+INSERT INTO tb_structure (species_id,organ_code,structure_id,name,name_en,type,organization_id,structure_size) VALUES ('2','0F','20F120','食管腔','Lumen','RO',1,1);
+INSERT INTO tb_structure (species_id,organ_code,structure_id,name,name_en,type,organization_id,structure_size) VALUES ('2','0F','20F13B','黏膜固有层+黏膜肌层+黏膜下层','Subepithelium','RO',1,1);
+INSERT INTO tb_structure (species_id,organ_code,structure_id,name,name_en,type,organization_id,structure_size) VALUES ('2','0F','20F111','组织轮廓','Tissue Area','RO',1,1);
+
+-- 结构标签初始化
+INSERT INTO tb_structure_tag ( structure_tag_set_id, structure_tag_name, structure_id, organization_id, create_by, update_by ) SELECT
+                                                                                                                                   b.structure_tag_set_id,
+                                                                                                                                   CONCAT( c.NAME, b.structure_tag_set_name, a.`name` ),
+                                                                                                                                   a.structure_id,
+                                                                                                                                   a.organization_id,
+                                                                                                                                   1,
+                                                                                                                                   1
+FROM
+    tb_structure a
+        LEFT JOIN tb_structure_tag_set b ON a.species_id = b.species_id
+        AND a.organ_code = b.organ_code
+        AND a.organization_id = b.organization_id
+        LEFT JOIN tb_species c ON b.species_id = c.species_id
+        AND b.organization_id = c.organization_id
+WHERE
+    a.species_id = '2';
+
+-- 结构标签颜色
+UPDATE tb_structure_tag SET rgb = 'rgb(255, 200, 255)',hex = '#FFC8FF' WHERE organization_id = 1 AND structure_id = '20F12E';
+UPDATE tb_structure_tag SET rgb = 'rgb(0, 255, 0)',hex = '#00FF00' WHERE organization_id = 1 AND structure_id = '20F12F';
+UPDATE tb_structure_tag SET rgb = 'rgb(77, 190, 238)',hex = '#4DBEEE' WHERE organization_id = 1 AND structure_id = '20F00C';
+UPDATE tb_structure_tag SET rgb = 'rgb(255, 128, 128)',hex = '#FF8080' WHERE organization_id = 1 AND structure_id = '20F120';
+UPDATE tb_structure_tag SET rgb = 'rgb(0, 0, 255)',hex = '#0000FF' WHERE organization_id = 1 AND structure_id = '20F13B';
+UPDATE tb_structure_tag SET rgb = 'rgb(162, 20, 47)',hex = '#A2142F' WHERE organization_id = 1 AND structure_id = '20F111';
